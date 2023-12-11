e14953653611ee7541c9e158ba1fcf177112c6af
Chelsea <chelsea.brown1@ca.ey.com>
2023-12-11 12:46:31 -0800

    [TC-0052-UI] frontend (basic) CSS/font files  (#4)
    * fonts, icons, basic components
    
    * fonts, icons, basic components
    
    * removed .env from repo (nothing secret in there so far)
    
    * installed pkgs (they are in another branch already - rebasing)

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,16 +1,7 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './styles/main.css';
 import App from './App';
 
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
+root.render(<App />);
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx,css}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['"BCSans"'],
+      },
+      colors: {
+        primary: '#003366',
+        // header, footer, beta status
+        secondary: '#fcba19',
+        // headings and paragraphs
+        text: '#313132',
+        // links
+        links: '#1a5a96',
+        // nav BG
+        bgNav: '#38598a',
+        //  text input, textarea, checkbox, radio button
+        formComponents: '#606060',
+        // semantic colours
+        error: '#d8292f',
+        success: '#2e8540',
+      },
+      backgroundColor: {
+        primary: '#f2f2f2',
+      },
+    },
   },
   plugins: [],
-}
+};
