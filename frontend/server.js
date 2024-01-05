046314d0ebb010b64956bbb37ff378d493231af7
Chelsea <chelsea.brown1@ca.ey.com>
2024-01-05 12:25:02 -0800

    [CHORE]: updates to react/express server (#28)
    * updates to react/express server
    
    * revert change in docker-compose

--- frontend/server.js
+++ frontend/server.js
@@ -1,10 +1,41 @@
 const path = require("path");
 const express = require("express");
-const app = express(); // create express app
-const port = process.env.PORT || 3000
+const compression = require('compression')
 
-app.use(express.static(path.join(__dirname, "build")));
-app.use(express.static(path.join(__dirname, "public")));
+const helmet = require("helmet");
+const RateLimit = require("express-rate-limit");
+//TODO verify the correct limit to set
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20,
+});
+
+const bodyParser = require('body-parser');
+const root = path.join(__dirname, 'build');
+
+const app = express()
+
+app.use(compression())
+app.use(bodyParser.json());
+
+app.use(express.static(root));
+app.use(limiter);
+
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      "script-src": ["'self'"],
+    },
+  }),
+);
+
+const port = process.env.PORT;
+console.log(process.env.NODE_ENV)
+
+app.use('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 
 app.listen(port, () => {
   console.log("server started on port" + `${port}`);
