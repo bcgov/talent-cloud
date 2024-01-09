e83a47b955daf4b29d17f951f8bda76932b80c24
Chelsea Brown <chelsea.brown1@ca.ey.com>
2024-01-08 18:49:10 -0800

    fix pipeline tests

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -2,12 +2,20 @@ import { defineConfig } from 'cypress';
 import customViteConfig from './vite.config';
 
 export default defineConfig({
-  component: {
+  e2e: {
+    baseUrl: 'http://localhost:3000',
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    }
+  },
+  component:{
     devServer: {
       framework: 'react',
       bundler: 'vite',
       // optionally pass in vite config
       viteConfig: customViteConfig,
     },
-  },
+  }, 
+  experimentalStudio: true,
+  experimentalWebKitSupport: true,
 });
