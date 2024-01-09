294cd8ee91e305b766fa2b054b3ae328762fe243
Chelsea Brown <chelsea.brown1@ca.ey.com>
2024-01-08 20:11:34 -0800

    fix lint

--- frontend/src/services/api-endpoints.tsx
+++ frontend/src/services/api-endpoints.tsx
@@ -1,4 +1,4 @@
-import { AxiosResponse } from 'axios';
+import type { AxiosResponse } from 'axios';
 import { AxiosPrivate } from '../utils';
 
 export const getUserInfo = async (): Promise<AxiosResponse> => {
