d7a36bd2531379225e7b1dd59a7e4bf77a90dac4
Noel Feliciano <noel.feliciano@ca.ey.com>
2024-01-15 15:03:02 -0800

    fix: remove public

--- backend/src/function/function.controller.ts
+++ backend/src/function/function.controller.ts
@@ -14,7 +14,6 @@ import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { QueryTransformPipe } from '../query-validation.pipe';
 import { FunctionService } from './function.service';
 import { FunctionRO } from './ro/function.ro';
-import { Public } from 'src/auth/public.decorator';
 
 @Controller('function')
 @ApiTags('Functions API')
@@ -34,7 +33,6 @@ export class FunctionController {
     type: [FunctionRO],
   })
   @Get()
-  @Public()
   @UsePipes(new QueryTransformPipe())
   async getFunctions(): Promise<FunctionRO[]> {
     const functions = await this.functionService.getFunctions();
