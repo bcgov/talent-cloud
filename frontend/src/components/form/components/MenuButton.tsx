69179473fbb4d6db0e0b42c54c3659c90901f9db
Chelsea Brown <chelsea.brown1@ca.ey.com>
2024-01-18 17:56:32 -0800

    move button

--- frontend/src/components/form/components/MenuButton.tsx
+++ frontend/src/components/form/components/MenuButton.tsx
@@ -38,13 +38,13 @@ export const MenuButton = ({
     <span className="text-gray-600 text-sm text-left flex flex-row items-center justify-between w-full">
       <div className="flex flex-row items-center justify-start">
         {renderComponent()}
-        <div className="relative">
-          {open ? (
-            <ChevronUpIcon className="h-5 w-5 text-dark" />
-          ) : (
-            <ChevronDownIcon className="h-5 w-5 text-dark" />
-          )}
-        </div>
+      </div>
+      <div>
+        {open ? (
+          <ChevronUpIcon className="h-5 w-5 text-dark" />
+        ) : (
+          <ChevronDownIcon className="h-5 w-5 text-dark" />
+        )}
       </div>
     </span>
   );
