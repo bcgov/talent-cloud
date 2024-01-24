e3b1d64cffc3f99677201fc4314f22ebd578b6b3
Chelsea Brown <chelsea.brown1@ca.ey.com>
2024-01-23 17:03:17 -0800

    ui bugfxes

--- frontend/src/hooks/useTable.tsx
+++ frontend/src/hooks/useTable.tsx
@@ -86,7 +86,7 @@ const useTable = () => {
                   {
                     key: uuidv4(),
                     columnName: DashboardColumns.LOCATION,
-                    value: workLocation,
+                    value: WorkLocation[workLocation as keyof typeof WorkLocation],
                     className: tableClass(
                       DashboardColumns.LOCATION,
                       workLocation?.toLowerCase(),
