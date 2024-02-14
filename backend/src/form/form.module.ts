b9297ca26402e49269e3e86d3e53be3508e8b1df
Chelsea Brown <chelsea.brown1@ca.ey.com>
2024-02-14 09:25:34 -0800

    saving formId and submissionId

--- backend/src/form/form.module.ts
+++ backend/src/form/form.module.ts
@@ -3,10 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { FormSubmissionController } from './form.controller';
 import { FormService } from './form.service';
 import { Form } from '../database/entities/form.entity';
+import { LoggerModule } from '../logger/logger.module';
 import { PersonnelModule } from '../personnel/personnel.module';
 
 @Module({
-  imports: [PersonnelModule, TypeOrmModule.forFeature([Form])],
+  imports: [LoggerModule, PersonnelModule, TypeOrmModule.forFeature([Form])],
   providers: [FormService],
   controllers: [FormSubmissionController],
   exports: [TypeOrmModule],
