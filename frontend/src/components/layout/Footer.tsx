c800fe5bd0f01361308e8a2addf5cd88d7e85270
Chelsea Brown <chelsea.brown1@ca.ey.com>
2024-02-21 11:11:20 -0800

    update links to open in a new tab - still missing the main llinks from splash page

--- frontend/src/components/layout/Footer.tsx
+++ frontend/src/components/layout/Footer.tsx
@@ -1,15 +1,20 @@
-import type { FooterProps, LinkProps } from '@/common';
-import { Link } from 'react-router-dom';
+import type { LinkProps } from '@/common';
+import { footerLinks } from '@/common/links';
 
-export const Footer = ({ links }: FooterProps) => {
+export const Footer = () => {
   return (
     <footer className="max-w-full bg-backgroundBlue mt-auto">
       <ul className="text-white   space-x-4 flex flex-row no-wrap justify-start items-center text-sm py-2 md:px-8 overflow-x-hidden">
-        {links.map(({ href, label }: LinkProps, i: number) => (
+        {footerLinks.map(({ href, label }: LinkProps, i: number) => (
           <li key={i.toString() + label}>
-            <Link to={href} className="border-r border-gray-100 pr-4">
+            <a
+              href={href}
+              className="border-r border-gray-100 pr-4"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               {label}
-            </Link>
+            </a>
           </li>
         ))}
       </ul>
