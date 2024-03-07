685174f0634100723482f96c3b1f986dbd535f32
Chelsea Brown <chelsea.brown1@ca.ey.com>
2024-03-07 15:04:01 -0800

    Added warning comment to helper function

--- backend/src/common/helpers.ts
+++ backend/src/common/helpers.ts
@@ -1,6 +1,7 @@
 /**
  *
  * @param date Used when calling new Date() on the backend to query for availability events saved with a PST date string
+ * !! DO NOT USE THIS FUNCTION WHEN SAVING DATES !!
  * @returns Date as PST date
  */
 export const datePST = (date: Date): Date => {
