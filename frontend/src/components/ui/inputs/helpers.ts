cae0c038f049df3fdbfb0248cda76e91d0a67997
Chelsea Brown <chelsea.brown1@ca.ey.com>
2024-03-13 11:08:11 -0700

    fix num format

--- frontend/src/components/ui/inputs/helpers.ts
+++ frontend/src/components/ui/inputs/helpers.ts
@@ -1,14 +1,16 @@
 export const formatPhone = (value: string) => {
   if (!value) return value;
   let currentValue = value.replace(/[^\d]/g, '');
-  if (currentValue.length >= 3 && currentValue.length < 5) {
-    currentValue = currentValue.replace(/(\d{3})/, '($1)');
-  }
-  if (currentValue.length >= 4 && currentValue.length < 10) {
-    currentValue = currentValue.replace(/(\d{3})(\d{3})/, '($1) $2-');
-  }
-  if (currentValue.length >= 10) {
+  console.log(currentValue.length, currentValue);
+  if (currentValue.length > 7) {
     currentValue = currentValue.replace(/(\d{3})(\d{3})(\d{4})/, '($1) $2-$3');
   }
+  if (currentValue.length > 6 && currentValue.length < 10) {
+    currentValue = currentValue.replace(/(\d{3})(\d{3})/, '($1) $2-');
+  }
+  if (currentValue.length > 2 && currentValue.length < 10) {
+    currentValue = currentValue.replace(/(\d{3})(\d)/, '($1) $2');
+  }
+
   return currentValue;
 };
