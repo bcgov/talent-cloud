b27f2d1485b8aa56a45d4948505f43029037d856
Noel Feliciano <noel.feliciano@ca.ey.com>
2024-03-18 21:38:14 -0700

    feat: file rotation saves for info and separate for errors

--- backend/src/logger/logger.module.ts
+++ backend/src/logger/logger.module.ts
@@ -2,9 +2,28 @@ import { Module } from '@nestjs/common';
 import { WinstonModule } from 'nest-winston';
 import { transports } from 'winston';
 import { AppLogger } from './logger.service';
+import 'winston-daily-rotate-file';
 
 @Module({
-  imports: [WinstonModule.forRoot(new transports.Console())],
+  imports: [WinstonModule.forRoot({
+    transports: [
+      new transports.Console(),
+      new transports.DailyRotateFile({
+        level: 'debug',
+        filename: 'logs-%DATE%.log',
+        datePattern: 'YYYY-MM-DD',
+        zippedArchive: true,
+        maxSize: '10m',
+        maxFiles: '14d',
+      }),
+      new transports.DailyRotateFile({
+        level: 'error',
+        filename: 'errors-%DATE%.log',
+        datePattern: 'YYYY-MM',
+        zippedArchive: true,
+      }),
+    ]
+  })],
   providers: [AppLogger],
   exports: [AppLogger],
 })
