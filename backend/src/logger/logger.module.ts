e850371fcc92e3d818ad30a77146ae7307b0bdef
Noel Feliciano <61991654+fw-noel@users.noreply.github.com>
2024-03-20 10:40:52 -0700

    chore: Remove daily rotate file (#180)

--- backend/src/logger/logger.module.ts
+++ backend/src/logger/logger.module.ts
@@ -2,28 +2,13 @@ import { Module } from '@nestjs/common';
 import { WinstonModule } from 'nest-winston';
 import { transports } from 'winston';
 import { AppLogger } from './logger.service';
-import 'winston-daily-rotate-file';
 
 @Module({
-  imports: [WinstonModule.forRoot({
-    transports: [
-      new transports.Console(),
-      new transports.DailyRotateFile({
-        level: 'debug',
-        filename: 'logs-%DATE%.log',
-        datePattern: 'YYYY-MM-DD',
-        zippedArchive: true,
-        maxSize: '10m',
-        maxFiles: '14d',
-      }),
-      new transports.DailyRotateFile({
-        level: 'error',
-        filename: 'errors-%DATE%.log',
-        datePattern: 'YYYY-MM',
-        zippedArchive: true,
-      }),
-    ]
-  })],
+  imports: [
+    WinstonModule.forRoot({
+      transports: [new transports.Console()],
+    }),
+  ],
   providers: [AppLogger],
   exports: [AppLogger],
 })
