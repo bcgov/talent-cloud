4f2989bbb3617cdab2239af954976246bf0957f3
Chelsea Brown <chelsea.brown1@ca.ey.com>
2024-03-27 15:55:12 -0700

    Date joined and application date showing correct date based on timezone

--- backend/src/database/migrations/1711575873472-datetype.ts
+++ backend/src/database/migrations/1711575873472-datetype.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class Migration1711575873472 implements MigrationInterface {
-    name = 'Migration1711575873472'
+    name = 'DateTypeMigration1711575873472'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "personnel" ALTER COLUMN "date_joined" type TIMESTAMP `);
