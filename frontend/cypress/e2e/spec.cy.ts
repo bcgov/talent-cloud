78aa074a4019320676d71ac16202258e06a8d86b
Noel Feliciano <noel.feliciano@ca.ey.com>
2024-04-01 14:39:30 -0700

    visit

--- frontend/cypress/e2e/spec.cy.ts
+++ frontend/cypress/e2e/spec.cy.ts
@@ -8,7 +8,7 @@ describe('local host spec', () => {
     cy.intercept({ method: 'GET', url: 'http://localhost:3000/api/v1/keycloak' }).as(
       'apicheck',
     );
-    // cy.visit('http://localhost:3000');
+    cy.visit('http://localhost:3000');
     cy.wait('@apicheck').then((interception) => {
       assert.isNotNull(interception.response.body, '1st API call has data');
     });
