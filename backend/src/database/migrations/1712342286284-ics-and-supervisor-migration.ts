d7456e02ea2f5220f6d64b752cb514bdd7dd8aa0
Noel Feliciano <noel.feliciano@ca.ey.com>
2024-04-09 14:57:29 -0700

    fix: approved by supervisor changes

--- backend/src/database/migrations/1712342286284-ics-and-supervisor-migration.ts
+++ backend/src/database/migrations/1712342286284-ics-and-supervisor-migration.ts
@@ -4,7 +4,8 @@ export class Migration1712342286284 implements MigrationInterface {
     name = 'ApprovedSupervisorMigration1712342286284'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "personnel" ADD "approved_by_supervisor" boolean`);
+        await queryRunner.query(`ALTER TABLE "personnel" ADD "approved_by_supervisor" boolean NOT NULL DEFAULT false`);
+        await queryRunner.query(`UPDATE "personnel" SET "approved_by_supervisor" = true WHERE status = 'ACTIVE'`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
