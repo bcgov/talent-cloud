bd52f74ba0173c8266a36a1b2709000c5fd10203
Chelsea Brown <chelsea.brown1@ca.ey.com>
2024-04-27 16:29:44 -0700

    logger module included in auth module

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { LoggerModule } from 'src/logger/logger.module';
 import { AuthController } from './auth.controller';
 import { AuthGuard } from './auth.guard';
 import { RolesGuard } from './roles.guard';
 
 @Module({
+  imports: [LoggerModule],
   controllers: [AuthController],
   providers: [AuthGuard, RolesGuard],
 })
