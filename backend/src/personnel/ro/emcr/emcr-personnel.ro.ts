acfa59ba27d47b7226e09182a5401d479781bf08
Chelsea Brown <chelsea.brown1@ca.ey.com>
2024-05-01 18:00:40 -0700

    WIP

--- backend/src/personnel/ro/emcr/emcr-personnel.ro.ts
+++ backend/src/personnel/ro/emcr/emcr-personnel.ro.ts
@@ -1,9 +1,7 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { Expose } from 'class-transformer';
 import { EmcrExperienceRO } from './experience.ro';
 import { EmcrLocationRO } from './region-location.ro';
 import { PersonnelRO } from '../personnel.ro';
-import { Status } from '../../../common/enums/status.enum';
 
 export class EmcrRO extends PersonnelRO {
   @ApiProperty({
@@ -29,57 +27,6 @@ export class EmcrRO extends PersonnelRO {
   homeLocation: EmcrLocationRO;
 
   @ApiProperty({
-    description: 'Date personnel applied',
-    required: true,
-    example: new Date(),
-  })
-  applicationDate: Date;
-
-  @ApiProperty({
-    description: "Personnel's noted skills and abilities",
-    required: false,
-    example: 'IT experience',
-  })
-  skillsAbilities: string;
-
-  @ApiProperty({
-    description: 'Notes about Personnel - Only vieweable by coordinators',
-    required: false,
-    example: 'MOTI Area Roads Mgr R24',
-  })
-  @Expose({ groups: ['coordinator'] })
-  coordinatorNotes: string;
-
-  @ApiProperty({
-    description: 'Notes about Personnel',
-    required: false,
-    example: 'MOTI Area Roads Mgr R24',
-  })
-  logisticsNotes: string;
-
-  @ApiProperty({
-    description: 'Has member been approved by supervisor',
-    required: true,
-    example: false,
-  })
-  approvedBySupervisor: boolean;
-
-  @ApiProperty({
-    description: 'Is Personnel active in the roster',
-    required: true,
-    example: true,
-  })
-  status: Status;
-
-  @ApiProperty({
-    description: 'Is Personnel newly approved',
-    required: false,
-    example: true,
-    default: false,
-  })
-  newMember: boolean;
-
-  @ApiProperty({
     description: 'Has finished ICS Training',
     required: true,
   })
@@ -92,10 +39,4 @@ export class EmcrRO extends PersonnelRO {
     type: () => EmcrExperienceRO,
   })
   experiences: EmcrExperienceRO[];
-
-  @ApiProperty({
-    description: 'Date of last deployment (last date of the deployment)',
-    required: false,
-  })
-  lastDeployedEMCR?: string;
 }
