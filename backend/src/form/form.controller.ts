cf32049d65a6f714fc26aaa522888e95c4988f7c
Chelsea Brown <chelsea.brown1@ca.ey.com>
2024-05-08 13:46:55 -0700

    uncomment form controller

--- backend/src/form/form.controller.ts
+++ backend/src/form/form.controller.ts
@@ -29,7 +29,7 @@ export class FormSubmissionController {
   @Token(TokenType.CHEFS)
   async handleIncomingEvents(@Req() req: RawBodyRequest<Request>) {
     this.logger.log('Received form submission event');
-    console.log(req.rawBody.toString());
+    // console.log(req.rawBody.toString());
     const rawBody = JSON.parse(req.rawBody.toString());
     await this.formService.processEventPayload(rawBody);
   }
