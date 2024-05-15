344b564d948eb4c0be57e964f1f9d32757255eb9
Chelsea Brown <chelsea.brown1@ca.ey.com>
2024-05-15 11:09:41 -0700

    update dev/test seeder

--- backend/src/database/migrations/1715368811077-insert-location-migration.ts
+++ backend/src/database/migrations/1715368811077-insert-location-migration.ts
@@ -14,101 +14,187 @@ export class Migration1715368811077 implements MigrationInterface {
     await queryRunner.query(
       `ALTER TABLE "bcws_personnel" DROP CONSTRAINT "FK_874eebc6d744a86c688612247c5"`,
     );
-
     await queryRunner.query(`
-        INSERT into location (fire_centre, region, location_name) values
-            ('CARIBOO',	'NEA',	'Quesnel'),
-            ('CARIBOO',	'NEA',	'Williams Lake'),
-            ('CARIBOO',	'NEA',	'Alexis Creek'),
-            ('COASTAL',	'HQ',	'Victoria'),
-            ('COASTAL',	'SWE',	'Abbotsford'),
-            ('COASTAL',	'SWE',	'Vancouver'),
-            ('COASTAL',	'VIC',	'Campbell River'),
-            ('COASTAL',	'VIC',	'Duncan'),
-            ('COASTAL',	'VIC',	'Nanaimo'),
-            ('COASTAL',	'VIC',	'Parksville'),
-            ('COASTAL',	'VIC',	'Port Alberni'),
-            ('COASTAL',	'VIC',	'Sechelt'),
-            ('COASTAL',	'VIC',	'Bella Coola'),
-            ('COASTAL',	'SWE',	'Boston Bar'),
-            ('COASTAL',	'SWE',	'Chilliwack'),
-            ('COASTAL',	'SWE',	'Cultus Lake'),
-            ('COASTAL',	'NWE',	'Masset'),
-            ('COASTAL',	'SWE',	'Pemberton'),
-            ('COASTAL',	'VIC',	'Powell River'),
-            ('COASTAL',	'SWE',	'Squamish'),
-            ('KAMLOOPS',	'CTL',	'Kamloops'),
-            ('KAMLOOPS',	'CTL',	'Kelowna'),
-            ('KAMLOOPS',	'CTL',	'Merritt'),
-            ('KAMLOOPS',	'CTL',	'Penticton'),
-            ('KAMLOOPS',	'CTL',	'Salmon Arm'),
-            ('KAMLOOPS',	'CTL',	'Vernon'),
-            ('KAMLOOPS',	'SWE',	'Lillooet'),
-            ('KAMLOOPS',	'CTL',	'Clearwater'),
-            ('KAMLOOPS',	'SWE',	'Lytton'),
-            ('KAMLOOPS',	'CTL',	'Princeton'),
-            ('NORTHWEST',	'NWE',	'Burns Lake'),
-            ('NORTHWEST',	'NWE',	'Smithers'),
-            ('NORTHWEST',	'NWE',	'Terrace'),
-            ('NORTHWEST',	'NWE',	'Atlin'),
-            ('NORTHWEST',	'NWE',	'Dease Lake'),
-            ('NORTHWEST',	'NWE',	'Hazelton'),
-            ('NORTHWEST',	'NWE',	'Houston'),
-            ('NORTHWEST',	'NWE',	'Prince Rupert'),
-            ('PRINCE_GEORGE',	'NEA',	'Dawson Creek'),
-            ('PRINCE_GEORGE',	'NEA',	'Fort Nelson'),
-            ('PRINCE_GEORGE',	'NEA',	'Fort St. John'),
-            ('PRINCE_GEORGE',	'NEA',	'Prince George'),
-            ('PRINCE_GEORGE',	'NWE',	'Vanderhoof'),
-            ('PRINCE_GEORGE',	'CTL',	'Blue River'),
-            ('PRINCE_GEORGE',	'NEA',	'Chetwynd'),
-            ('PRINCE_GEORGE',	'NEA',	'Fort St. James'),
-            ('PRINCE_GEORGE',	'NEA',	'MacKenzie'),
-            ('PRINCE_GEORGE',	'NEA',	'Valemount'),
-            ('SOUTHEAST',	'SEA',	'Cranbrook'),
-            ('SOUTHEAST',	'SEA',	'Kimberley'),
-            ('SOUTHEAST',	'SEA',	'Nelson'),
-            ('SOUTHEAST',	'SEA',	'Castelgar'),
-            ('SOUTHEAST',	'SEA',	'Golden'),
-            ('SOUTHEAST',	'SEA',	'Grand Forks'),
-            ('SOUTHEAST',	'SEA',	'Invermere'),
-            ('SOUTHEAST',	'SEA',	'Nakusp'),
-            ('KAMLOOPS',	'CTL',	'Enderby'),
-            ('KAMLOOPS',	'CTL',	'Sorrento'),
-            ('COASTAL',	'HQ',	'Brentwood Bay'),
-            ('COASTAL',	'HQ',	'Esquimalt'),
-            ('COASTAL',	'HQ',	'Langford'),
-            ('COASTAL',	'HQ',	'Saanich'),
-            ('COASTAL',	'HQ',	'Saanichton'),
-            ('COASTAL',	'HQ',	'Sidney'),
-            ('CARIBOO',	'NEA',	'100 Mile House'),
-            ('CARIBOO',	'NEA',	'150 Mile House'),
-            ('PRINCE_GEORGE',	'NEA',	'Mackenzie'),
-            ('SOUTHEAST',	'SEA',	'Bonnington Falls'),
-            ('SOUTHEAST',	'SEA',	'Castlegar'),
-            ('SOUTHEAST',	'SEA',	'Elkford'),
-            ('SOUTHEAST',	'SEA',	'Revelstoke'),
-            ('COASTAL',	'SWE',	'Burnaby'),
-            ('COASTAL',	'SWE',	'Coquitlam'),
-            ('COASTAL',	'SWE',	'Langley'),
-            ('COASTAL',	'SWE',	'Maple Ridge'),
-            ('COASTAL',	'SWE',	'Mission'),
-            ('COASTAL',	'SWE',	'New Westminster'),
-            ('COASTAL',	'SWE',	'North Vancouver'),
-            ('COASTAL',	'SWE',	'Richmond'),
-            ('COASTAL',	'SWE',	'Surrey'),
-            ('COASTAL',	'SWE',	'Whistler'),
-            ('COASTAL',	'VIC',	'Courtenay'),
-            ('COASTAL',	'VIC',	'Cumberland'),
-            ('COASTAL',	'VIC',	'Mill Bay'),
-            ('COASTAL',	'VIC',	'Port McNeil'),
-            ('COASTAL',	'VIC',	'Qualicum Beach'),
-            ('COASTAL',	'VIC',	'Sooke'),
-            ('COASTAL',	'VIC',	'Ucluelet')
-            ON CONFLICT (location_name) DO UPDATE
-            SET region = EXCLUDED.region, fire_centre=excluded.fire_centre;
-    `);
-
+    INSERT into location (region, location_name) values
+        (	'NEA',	'Quesnel'),
+        (	'NEA',	'Williams Lake'),
+        (	'NEA',	'Alexis Creek'),
+        (	'HQ',	'Victoria'),
+        (	'SWE',	'Abbotsford'),
+        (	'SWE',	'Vancouver'),
+        (	'VIC',	'Campbell River'),
+        (	'VIC',	'Duncan'),
+        (	'VIC',	'Nanaimo'),
+        (	'VIC',	'Parksville'),
+        (	'VIC',	'Port Alberni'),
+        (	'VIC',	'Sechelt'),
+        (	'VIC',	'Bella Coola'),
+        (	'SWE',	'Boston Bar'),
+        (	'SWE',	'Chilliwack'),
+        (	'SWE',	'Cultus Lake'),
+        (	'NWE',	'Masset'),
+        (	'SWE',	'Pemberton'),
+        (	'VIC',	'Powell River'),
+        (	'SWE',	'Squamish'),
+        (	'CTL',	'Kamloops'),
+        (	'CTL',	'Kelowna'),
+        (	'CTL',	'Merritt'),
+        (	'CTL',	'Penticton'),
+        (	'CTL',	'Salmon Arm'),
+        (	'CTL',	'Vernon'),
+        (	'SWE',	'Lillooet'),
+        (	'CTL',	'Clearwater'),
+        (	'SWE',	'Lytton'),
+        (	'CTL',	'Princeton'),
+        (	'NWE',	'Burns Lake'),
+        (	'NWE',	'Smithers'),
+        (	'NWE',	'Terrace'),
+        (	'NWE',	'Atlin'),
+        (	'NWE',	'Dease Lake'),
+        (	'NWE',	'Hazelton'),
+        (	'NWE',	'Houston'),
+        (	'NWE',	'Prince Rupert'),
+        (	'NEA',	'Dawson Creek'),
+        (	'NEA',	'Fort Nelson'),
+        (	'NEA',	'Fort St. John'),
+        (	'NEA',	'Prince George'),
+        (	'NWE',	'Vanderhoof'),
+        (	'CTL',	'Blue River'),
+        (	'NEA',	'Chetwynd'),
+        (	'NEA',	'Fort St. James'),
+        (	'NEA',	'MacKenzie'),
+        (	'NEA',	'Valemount'),
+        (	'SEA',	'Cranbrook'),
+        (	'SEA',	'Kimberley'),
+        (	'SEA',	'Nelson'),
+        (	'SEA',	'Castelgar'),
+        (	'SEA',	'Golden'),
+        (	'SEA',	'Grand Forks'),
+        (	'SEA',	'Invermere'),
+        (	'SEA',	'Nakusp'),
+        (	'CTL',	'Enderby'),
+        (	'CTL',	'Sorrento'),
+        (	'HQ',	'Brentwood Bay'),
+        (	'HQ',	'Esquimalt'),
+        (	'HQ',	'Langford'),
+        (	'HQ',	'Saanich'),
+        (	'HQ',	'Saanichton'),
+        (	'HQ',	'Sidney'),
+        (	'NEA',	'100 Mile House'),
+        (	'NEA',	'150 Mile House'),
+        (	'NEA',	'Mackenzie'),
+        (	'SEA',	'Bonnington Falls'),
+        (	'SEA',	'Castlegar'),
+        (	'SEA',	'Elkford'),
+        (	'SEA',	'Revelstoke'),
+        (	'SWE',	'Burnaby'),
+        (	'SWE',	'Coquitlam'),
+        (	'SWE',	'Langley'),
+        (	'SWE',	'Maple Ridge'),
+        (	'SWE',	'Mission'),
+        (	'SWE',	'New Westminster'),
+        (	'SWE',	'North Vancouver'),
+        (	'SWE',	'Richmond'),
+        (	'SWE',	'Surrey'),
+        (	'SWE',	'Whistler'),
+        (	'VIC',	'Courtenay'),
+        (	'VIC',	'Cumberland'),
+        (	'VIC',	'Mill Bay'),
+        (	'VIC',	'Port McNeil'),
+        (	'VIC',	'Qualicum Beach'),
+        (	'VIC',	'Sooke'),
+        (	'VIC',	'Ucluelet')
+        ON CONFLICT DO NOTHING
+        
+`);
+    await queryRunner.query(`UPDATE location SET fire_centre='CARIBOO' WHERE location_name=	'Quesnel'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='CARIBOO' WHERE location_name=	'Williams Lake'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='CARIBOO' WHERE location_name=	'Alexis Creek'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='COASTAL' WHERE location_name=	'Victoria'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='COASTAL' WHERE location_name=	'Abbotsford'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='COASTAL' WHERE location_name=	'Vancouver'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='COASTAL' WHERE location_name=	'Campbell River'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='COASTAL' WHERE location_name=	'Duncan'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='COASTAL' WHERE location_name=	'Nanaimo'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='COASTAL' WHERE location_name=	'Parksville'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='COASTAL' WHERE location_name=	'Port Alberni'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='COASTAL' WHERE location_name=	'Sechelt'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='COASTAL' WHERE location_name=	'Bella Coola'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='COASTAL' WHERE location_name=	'Boston Bar'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='COASTAL' WHERE location_name=	'Chilliwack'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='COASTAL' WHERE location_name=	'Cultus Lake'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='COASTAL' WHERE location_name=	'Masset'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='COASTAL' WHERE location_name=	'Pemberton'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='COASTAL' WHERE location_name=	'Powell River'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='COASTAL' WHERE location_name=	'Squamish'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='KAMLOOPS' WHERE location_name=	'Kamloops'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='KAMLOOPS' WHERE location_name=	'Kelowna'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='KAMLOOPS' WHERE location_name=	'Merritt'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='KAMLOOPS' WHERE location_name=	'Penticton'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='KAMLOOPS' WHERE location_name=	'Salmon Arm'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='KAMLOOPS' WHERE location_name=	'Vernon'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='KAMLOOPS' WHERE location_name=	'Lillooet'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='KAMLOOPS' WHERE location_name=	'Clearwater'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='KAMLOOPS' WHERE location_name=	'Lytton'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='KAMLOOPS' WHERE location_name=	'Princeton'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='NORTHWEST' WHERE location_name=	'Burns Lake'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='NORTHWEST' WHERE location_name=	'Smithers'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='NORTHWEST' WHERE location_name=	'Terrace'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='NORTHWEST' WHERE location_name=	'Atlin'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='NORTHWEST' WHERE location_name=	'Dease Lake'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='NORTHWEST' WHERE location_name=	'Hazelton'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='NORTHWEST' WHERE location_name=	'Houston'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='NORTHWEST' WHERE location_name=	'Prince Rupert'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='PRINCE_GEORGE' WHERE location_name=	'Dawson Creek'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='PRINCE_GEORGE' WHERE location_name=	'Fort Nelson'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='PRINCE_GEORGE' WHERE location_name=	'Fort St. John'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='PRINCE_GEORGE' WHERE location_name=	'Prince George'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='PRINCE_GEORGE' WHERE location_name=	'Vanderhoof'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='PRINCE_GEORGE' WHERE location_name=	'Blue River'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='PRINCE_GEORGE' WHERE location_name=	'Chetwynd'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='PRINCE_GEORGE' WHERE location_name=	'Fort St. James'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='PRINCE_GEORGE' WHERE location_name=	'MacKenzie'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='PRINCE_GEORGE' WHERE location_name=	'Valemount'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='SOUTHEAST' WHERE location_name=	'Cranbrook'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='SOUTHEAST' WHERE location_name=	'Kimberley'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='SOUTHEAST' WHERE location_name=	'Nelson'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='SOUTHEAST' WHERE location_name=	'Castelgar'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='SOUTHEAST' WHERE location_name=	'Golden'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='SOUTHEAST' WHERE location_name=	'Grand Forks'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='SOUTHEAST' WHERE location_name=	'Invermere'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='SOUTHEAST' WHERE location_name=	'Nakusp'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='KAMLOOPS' WHERE location_name=	'Enderby'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='KAMLOOPS' WHERE location_name=	'Sorrento'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='COASTAL' WHERE location_name=	'Brentwood Bay'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='COASTAL' WHERE location_name=	'Esquimalt'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='COASTAL' WHERE location_name=	'Langford'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='COASTAL' WHERE location_name=	'Saanich'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='COASTAL' WHERE location_name=	'Saanichton'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='COASTAL' WHERE location_name=	'Sidney'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='CARIBOO' WHERE location_name=	'100 Mile House'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='CARIBOO' WHERE location_name=	'150 Mile House'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='PRINCE_GEORGE' WHERE location_name=	'Mackenzie'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='SOUTHEAST' WHERE location_name=	'Bonnington Falls'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='SOUTHEAST' WHERE location_name=	'Castlegar'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='SOUTHEAST' WHERE location_name=	'Elkford'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='SOUTHEAST' WHERE location_name=	'Revelstoke'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='COASTAL' WHERE location_name=	'Burnaby'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='COASTAL' WHERE location_name=	'Coquitlam'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='COASTAL' WHERE location_name=	'Langley'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='COASTAL' WHERE location_name=	'Maple Ridge'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='COASTAL' WHERE location_name=	'Mission'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='COASTAL' WHERE location_name=	'New Westminster'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='COASTAL' WHERE location_name=	'North Vancouver'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='COASTAL' WHERE location_name=	'Richmond'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='COASTAL' WHERE location_name=	'Surrey'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='COASTAL' WHERE location_name=	'Whistler'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='COASTAL' WHERE location_name=	'Courtenay'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='COASTAL' WHERE location_name=	'Cumberland'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='COASTAL' WHERE location_name=	'Mill Bay'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='COASTAL' WHERE location_name=	'Port McNeil'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='COASTAL' WHERE location_name=	'Qualicum Beach'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='COASTAL' WHERE location_name=	'Sooke'`);
+    await queryRunner.query(`UPDATE location SET fire_centre='COASTAL' WHERE location_name=	'Ucluelet'`);
     await queryRunner.query(
       `ALTER TABLE "bcws_personnel" ADD CONSTRAINT "FK_874eebc6d744a86c688612247c5" FOREIGN KEY ("work_fire_centre") REFERENCES "location"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
     );
