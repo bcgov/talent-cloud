560df5d8c45423da8c1cac7d13d844237b6f4003
Chelsea Brown <chelsea.brown1@ca.ey.com>
2024-05-16 14:00:05 -0700

    fixed seed cmd bcws

--- backend/src/database/migrations/1715887048702-language-id-migration.ts
+++ backend/src/database/migrations/1715887048702-language-id-migration.ts
@@ -6,23 +6,21 @@ export class LanguageIDMigration1715887048702 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "bcws_personnel_language" ADD "id" SERIAL NOT NULL`);
         await queryRunner.query(`ALTER TABLE "bcws_personnel_language" DROP CONSTRAINT "PK_84e834ee0e921d47e11b5c6bd29"`);
-        await queryRunner.query(`ALTER TABLE "bcws_personnel_language" ADD CONSTRAINT "PK_c45238c0fe98184ab88c981677d" PRIMARY KEY ("personnel_id", "id")`);
-        await queryRunner.query(`ALTER TABLE "bcws_personnel_language" DROP CONSTRAINT "FK_84e834ee0e921d47e11b5c6bd29"`);
-        await queryRunner.query(`ALTER TABLE "bcws_personnel_language" DROP CONSTRAINT "PK_c45238c0fe98184ab88c981677d"`);
+        
         await queryRunner.query(`ALTER TABLE "bcws_personnel_language" ALTER COLUMN "personnel_id" DROP NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "bcws_personnel_language" DROP CONSTRAINT "FK_84e834ee0e921d47e11b5c6bd29"`);
+    
         await queryRunner.query(`ALTER TABLE "bcws_personnel_language" ADD CONSTRAINT "PK_e37ad1c812172d33a1b7d3762fb" PRIMARY KEY ("id")`);
-        await queryRunner.query(`ALTER TABLE "bcws_personnel_language" ADD CONSTRAINT "FK_84e834ee0e921d47e11b5c6bd29" FOREIGN KEY ("personnel_id") REFERENCES "bcws_personnel"("personnel_id") ON DELETE NO ACTION ON UPDATE NO ACTION`);  
-        
-        
+
+        await queryRunner.query(`ALTER TABLE "bcws_personnel_language" ADD CONSTRAINT "FK_84e834ee0e921d47e11b5c6bd29" FOREIGN KEY ("personnel_id") REFERENCES "bcws_personnel"("personnel_id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "bcws_personnel_language" DROP CONSTRAINT "FK_84e834ee0e921d47e11b5c6bd29"`);
         await queryRunner.query(`ALTER TABLE "bcws_personnel_language" DROP CONSTRAINT "PK_e37ad1c812172d33a1b7d3762fb"`);
-        await queryRunner.query(`ALTER TABLE "bcws_personnel_language" ADD CONSTRAINT "PK_c45238c0fe98184ab88c981677d" PRIMARY KEY ("personnel_id", "id")`);
-        await queryRunner.query(`ALTER TABLE "bcws_personnel_language" ALTER COLUMN "personnel_id" SET NOT NULL`);
+        
         await queryRunner.query(`ALTER TABLE "bcws_personnel_language" ADD CONSTRAINT "FK_84e834ee0e921d47e11b5c6bd29" FOREIGN KEY ("personnel_id") REFERENCES "bcws_personnel"("personnel_id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
-        await queryRunner.query(`ALTER TABLE "bcws_personnel_language" DROP CONSTRAINT "PK_c45238c0fe98184ab88c981677d"`);
+        await queryRunner.query(`ALTER TABLE "bcws_personnel_language" ALTER COLUMN "personnel_id" SET NOT NULL`);
         await queryRunner.query(`ALTER TABLE "bcws_personnel_language" ADD CONSTRAINT "PK_84e834ee0e921d47e11b5c6bd29" PRIMARY KEY ("personnel_id")`);
         await queryRunner.query(`ALTER TABLE "bcws_personnel_language" DROP COLUMN "id"`);
     }
