51916d54c49bd368ba307e00a96e08e3ccb7d443
Chelsea <chelsea.brown1@ca.ey.com>
2024-05-30 18:50:44 -0700

    fix profile edit dropdownfor driver license (#268)

--- frontend/src/components/ui/form/FormField.tsx
+++ frontend/src/components/ui/form/FormField.tsx
@@ -15,22 +15,39 @@ export const FormField = ({
     useFormikContext<Partial<Personnel>>();
   const fieldProps = { ...getFieldProps(field.name) };
 
+  //TODO change Personnel to a generic type
+
   return (
     <>
       {field.type === 'text' || field.type === 'tel' ? (
         <TextInput {...fieldProps} {...field} onChange={handleChange} />
       ) : field.multiple ? (
         <MultiSelect
-          field={field}
-          values={values[field.name as keyof typeof values]}
+          field={
+            field as {
+              name: string;
+              placeholder: string;
+              options: { label: string; value: string }[];
+            }
+          }
+          values={values[field.name as keyof Personnel] as string[]}
           handleClose={(name, value) =>
-            setValues({ ...values, [name]: fieldProps.value.filter(value) })
+            setValues({
+              ...values,
+              [name]: (values[name as keyof Personnel] as string[]).filter(
+                (itm: string) => itm !== value,
+              ),
+            })
           }
           handleCloseMany={(name) => setValues({ ...values, [name]: [] })}
           maxChips={0}
-          {...fieldProps}
-          {...field}
-          onChange={handleChange}
+          handleChange={(name: string, value: string) =>
+            setValues({
+              ...values,
+              [name]: [...(values[name as keyof Personnel] as string[]), value],
+            })
+          }
+          label={field.label}
         />
       ) : (
         <Select {...fieldProps} {...field} onChange={handleChange} />
