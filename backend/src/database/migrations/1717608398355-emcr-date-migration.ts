5ced14fbdc4512425ab5d77a9f4be3a2d4928ec6
Chelsea Brown <chelsea.brown1@ca.ey.com>
2024-06-06 10:32:00 -0700

    Fix emcr date migration

--- backend/src/database/migrations/1717608398355-emcr-date-migration.ts
+++ backend/src/database/migrations/1717608398355-emcr-date-migration.ts
@@ -5,18 +5,18 @@ export class EmcrDateMigration1717608398355 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
     
-        await queryRunner.query(`update emcr_personnel ep set date_approved = (select date_approved from emcr_personnel ep2 where ep2.personnel_id = ep.personnel_id) - interval '7 hour' where date_approved is not null;`)
+        await queryRunner.query(`update emcr_personnel ep set date_approved = (select date_approved from emcr_personnel ep2 where ep2.personnel_id = ep.personnel_id) + interval '12 hour' where date_approved is not null;`)
 
 
-        await queryRunner.query(`update emcr_personnel ep set date_applied = (select date_applied from emcr_personnel ep2 where ep2.personnel_id = ep.personnel_id) - interval '7 hour' where date_applied is not null;`)
+        await queryRunner.query(`update emcr_personnel ep set date_applied = (select date_applied from emcr_personnel ep2 where ep2.personnel_id = ep.personnel_id) + interval '12 hour' where date_applied is not null;`)
 
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         
-        await queryRunner.query(`update emcr_personnel ep set date_approved = (select date_approved from emcr_personnel ep2 where ep2.personnel_id = ep.personnel_id) + interval '7 hour' where date_approved is not null;`)
+        await queryRunner.query(`update emcr_personnel ep set date_approved = (select date_approved from emcr_personnel ep2 where ep2.personnel_id = ep.personnel_id) - interval '12 hour' where date_approved is not null;`)
 
-        await queryRunner.query(`update emcr_personnel ep set date_applied = (select date_applied from emcr_personnel ep2 where ep2.personnel_id = ep.personnel_id) + interval '7 hour' where date_applied is not null;`)
+        await queryRunner.query(`update emcr_personnel ep set date_applied = (select date_applied from emcr_personnel ep2 where ep2.personnel_id = ep.personnel_id) - interval '12 hour' where date_applied is not null;`)
     }
 
 }
