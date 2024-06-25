fe432c95fa4455718fcaf091b2aaa5a578dff732
Chelsea Brown <chelsea.brown1@ca.ey.com>
2024-06-25 09:56:16 -0700

    ignoring cooridnator notes in RO for logistics

--- backend/src/personnel/ro/emcr/emcr-personnel.ro.ts
+++ backend/src/personnel/ro/emcr/emcr-personnel.ro.ts
@@ -1,8 +1,24 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { Expose } from 'class-transformer';
 import { EmcrExperienceRO } from './experience.ro';
 import { PersonnelRO } from '../personnel.ro';
 
 export class EmcrRO extends PersonnelRO {
+  @ApiProperty({
+    description: 'Notes about Personnel - Only vieweable by coordinators',
+    required: false,
+    example: 'MOTI Area Roads Mgr R24',
+  })
+  @Expose({ groups: ['coordinator'] })
+  coordinatorNotes: string;
+
+  @ApiProperty({
+    description: 'Notes about Personnel',
+    required: false,
+    example: 'MOTI Area Roads Mgr R24',
+  })
+  logisticsNotes: string;
+
   @ApiProperty({
     description: 'Has finished ICS Training',
     required: true,
