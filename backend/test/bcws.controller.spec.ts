9ed7bfd7b93aeb6c5b247a927d59a7416dc10530
Chelsea Brown <chelsea.brown1@ca.ey.com>
2024-06-25 09:58:40 -0700

    fix tets import

--- backend/test/bcws.controller.spec.ts
+++ backend/test/bcws.controller.spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { BcwsController } from './bcws.controller';
+import { BcwsController } from '../src/bcws/bcws.controller';
 
 describe('BcwsController', () => {
   let controller: BcwsController;
