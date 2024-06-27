57de757078564b047074489906b6e4513d223274
Chelsea Brown <chelsea.brown1@ca.ey.com>
2024-06-26 17:10:12 -0700

    remove duplicate fiel

--- frontend/src/pages/profile/types.ts
+++ frontend/src/pages/profile/types.ts
@@ -36,5 +36,5 @@ export type DetailProps = {
   pending: boolean;
   intakeRequirements?: ProfileDetail[];
   openEditProfilePopUp: (e: React.MouseEvent<HTMLElement>) => void;
-  allowEditing: boolean;
+  
 };
