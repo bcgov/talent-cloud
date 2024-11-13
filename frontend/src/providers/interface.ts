f70eaa21d9c394950058a9b10598dd14b7d165bc
chelsea-EYDS <chelsea.brown1@ca.ey.com>
2024-11-12 17:50:05 -0800

    FE login flow

--- frontend/src/providers/interface.ts
+++ frontend/src/providers/interface.ts
@@ -3,9 +3,8 @@ import {Program, Role, Status } from "@/common";
 export interface User {
   program?: Program;
   role?: Role;
-  status?: Status;
-  username: string;
-  idir: string;
-  loading: boolean;
+  username?: string;
+  idir?: string;
+  
 }
 
