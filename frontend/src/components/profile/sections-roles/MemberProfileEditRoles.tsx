0e90c7a85e19870a9e918068024fcd872615c998
Noel Feliciano <noel.feliciano@ca.ey.com>
2024-12-11 13:06:21 -0800

    emcr functions saving

--- frontend/src/components/profile/sections-roles/MemberProfileEditRoles.tsx
+++ frontend/src/components/profile/sections-roles/MemberProfileEditRoles.tsx
@@ -1,27 +1,44 @@
-import { Fragment, useState } from 'react';
-import { Button } from '@/components';
-import { ButtonTypes } from '@/common';
-import type { BcwsPersonnelRoleInterface, BcwsRoleInterface } from '@/common';
+import type {
+  BcwsPersonnelRoleInterface,
+  BcwsRoleInterface,
+  ExperienceInterface,
+  FunctionType,
+} from '@/common';
+import { ButtonTypes, Experience } from '@/common';
 import {
   BcwsRoleName,
   ExperienceLevel,
   Section,
   SectionName,
 } from '@/common/enums/sections.enum';
-import { ProfileSectionHeader } from '../common';
+import { Button } from '@/components';
 import {
   Combobox,
   ComboboxInput,
   ComboboxOption,
   ComboboxOptions,
 } from '@headlessui/react';
-import { ChevronDownIcon } from '@heroicons/react/24/solid';
 import { XMarkIcon } from '@heroicons/react/24/outline';
+import { ChevronDownIcon } from '@heroicons/react/24/solid';
+import { Fragment, useEffect, useState } from 'react';
+import { ProfileSectionHeader } from '../common';
 
 interface RoleSelectProps {
   allRoles: BcwsRoleInterface[];
   selectedRoles: BcwsRoleInterface[];
   onRoleSelect: (roles: BcwsRoleInterface[]) => void;
+}
+
+interface RoleChanges {
+  firstChoiceSection?: Section;
+  secondChoiceSection?: Section;
+  roles: BcwsRoleInterface[];
+}
+
+interface FunctionChanges {
+  firstChoiceSection?: string;
+  secondChoiceSection?: string;
+  functions: FunctionType[];
 }
 
 const RoleSelect = ({ allRoles, selectedRoles, onRoleSelect }: RoleSelectProps) => {
@@ -109,18 +126,94 @@ const RoleSelect = ({ allRoles, selectedRoles, onRoleSelect }: RoleSelectProps) 
   );
 };
 
-export const MemberProfileEditRoles = ({
+const FunctionSelect = ({
+  allFunctions,
+  selectedFunctions,
+  onFunctionSelect,
+}: {
+  allFunctions: FunctionType[];
+  selectedFunctions: FunctionType[];
+  onFunctionSelect: (functions: FunctionType[]) => void;
+}) => {
+  const [query, setQuery] = useState('');
+  const [isOpen, setIsOpen] = useState(false);
+
+  const filteredFunctions =
+    query === ''
+      ? allFunctions
+      : allFunctions.filter(
+          (func) =>
+            func.name.toLowerCase().includes(query.toLowerCase()) ||
+            func.abbreviation.toLowerCase().includes(query.toLowerCase()),
+        );
+
+  const isFunctionSelected = (func: FunctionType) => {
+    return selectedFunctions.some(
+      (selectedFunction) => selectedFunction.id === func.id,
+    );
+  };
+
+  return (
+    <Combobox
+      immediate
+      value={selectedFunctions}
+      onChange={onFunctionSelect}
+      multiple
+      open={isOpen}
+      onFocus={() => setIsOpen(true)}
+      onBlur={() => setIsOpen(false)}
+    >
+      <div className="relative mt-1">
+        <div className="relative w-full cursor-default overflow-hidden rounded-lg bg-white text-left border focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
+          <ComboboxInput
+            className="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
+            placeholder="Search for a section"
+            onChange={(event) => setQuery(event.target.value)}
+          />
+          <div className="absolute inset-y-0 right-0 flex items-center pr-2">
+            <ChevronDownIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+          </div>
+        </div>
+        <ComboboxOptions className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+          {filteredFunctions.map((func) => (
+            <ComboboxOption
+              key={func.id}
+              value={func}
+              className={`relative cursor-default select-none py-2 px-4 text-defaultGray data-[focus]:bg-calBlue`}
+            >
+              {() => (
+                <div className="flex items-center">
+                  <input
+                    type="checkbox"
+                    checked={isFunctionSelected(func)}
+                    className="h-4 w-4 border-defaultGray border-2 text-primaryBlue focus:ring-primaryBlue mr-3"
+                    readOnly
+                  />
+                  <span
+                    className={`block text-defaultGray truncate ${isFunctionSelected(func) ? 'font-medium' : 'font-normal'}`}
+                  >
+                    {func.name}
+                  </span>
+                </div>
+              )}
+            </ComboboxOption>
+          ))}
+        </ComboboxOptions>
+      </div>
+    </Combobox>
+  );
+};
+
+const MemberProfileEditRoles = ({
   allRoles,
   originalRoles,
   sectionChoices,
-  handleClose,
-  handleSave,
+  handleChange,
 }: {
   allRoles: BcwsRoleInterface[];
   originalRoles: BcwsPersonnelRoleInterface[];
   sectionChoices: { firstChoiceSection?: Section; secondChoiceSection?: Section };
-  handleClose: () => void;
-  handleSave: (personnel: any) => Promise<void>;
+  handleChange: (rolesToSave: RoleChanges) => void;
 }) => {
   const [currentRoles, setCurrentRoles] = useState<BcwsRoleInterface[]>(
     originalRoles.map((r) => ({
@@ -136,37 +229,17 @@ export const MemberProfileEditRoles = ({
     sectionChoices.secondChoiceSection,
   );
 
-  const onSave = async () => {
-    const newRoles = currentRoles.filter(
-      (cr) => !originalRoles.map((or) => or.id).includes(cr.id),
-    );
-    const rolesExcludingRemoved = originalRoles
-      .filter((or) => currentRoles.map((cr) => cr.id).includes(or.id))
-      .map((r) => ({
-        roleId: r.id,
-        expLevel: r.expLevel,
-      }));
-    const rolesToSave = [
-      ...newRoles.map((r) => ({
-        roleId: r.id,
-        expLevel: ExperienceLevel.INTERESTED,
-      })),
-      ...rolesExcludingRemoved,
-    ];
-    const personnelUpdate = {
-      bcws: {
-        firstChoiceSection,
-        secondChoiceSection,
-        roles: rolesToSave,
-      },
-    };
-    await handleSave(personnelUpdate);
-    handleClose();
-  };
+  useEffect(() => {
+    handleChange({
+      firstChoiceSection,
+      secondChoiceSection,
+      roles: currentRoles,
+    });
+  }, [currentRoles, firstChoiceSection, secondChoiceSection]);
 
   return (
-    <div className="pb-6">
-      <div className="px-12 pt-6">
+    <>
+      <div className="px-12 pt-4">
         <ProfileSectionHeader title="Rank your Top 3 Sections">
           <div className="flex flex-row gap-8">
             <div className="flex flex-col basis-1/2">
@@ -203,12 +276,15 @@ export const MemberProfileEditRoles = ({
             </div>
           </div>
         </ProfileSectionHeader>
-        <div className="border-b-2 border-defaultGray my-6"></div>
-        <ProfileSectionHeader title="Add New Role">
+        <ProfileSectionHeader title="Add New Role(s)">
           <>
             <p className="font-bold text-sm">Select a Role</p>
             <RoleSelect
-              allRoles={allRoles}
+              allRoles={allRoles.sort(
+                (a, b) =>
+                  Object.values(Section).indexOf(b.section) -
+                  Object.values(Section).indexOf(a.section),
+              )}
               selectedRoles={currentRoles}
               onRoleSelect={(roles) => setCurrentRoles(roles)}
             />
@@ -243,6 +319,254 @@ export const MemberProfileEditRoles = ({
           </>
         </ProfileSectionHeader>
       </div>
+    </>
+  );
+};
+
+const MemberProfileEditFunctions = ({
+  allFunctions,
+  originalExperiences,
+  sectionChoices,
+  handleChange,
+}: {
+  allFunctions: FunctionType[];
+  originalExperiences: ExperienceInterface[];
+  sectionChoices: {
+    firstChoiceSection?: string;
+    secondChoiceSection?: string;
+    thirdChoiceSection?: string;
+  };
+  handleChange: (functionsToChange: FunctionChanges) => void;
+}) => {
+  const [firstChoiceSection, setFirstChoiceSection] = useState(
+    sectionChoices.firstChoiceSection,
+  );
+  const [secondChoiceSection, setSecondChoiceSection] = useState(
+    sectionChoices.secondChoiceSection,
+  );
+  const [currentFunctions, setCurrentFunctions] = useState<FunctionType[]>(
+    originalExperiences.map((e) => ({
+      id: e.function.id,
+      name: e.function.name,
+      abbreviation: e.function.abbreviation,
+    })),
+  );
+
+  useEffect(() => {
+    handleChange({
+      firstChoiceSection,
+      secondChoiceSection,
+      functions: currentFunctions,
+    });
+  }, [currentFunctions, firstChoiceSection, secondChoiceSection]);
+
+  return (
+    <>
+      <div className="px-12 pt-4">
+        <ProfileSectionHeader title="Rank your Top 3 Sections">
+          <div className="flex flex-row gap-8">
+            <div className="flex flex-col basis-1/2">
+              <p className="font-bold text-sm pb-2">
+                1st Choice
+                <span className="text-red-300">*</span>
+              </p>
+              <select
+                value={firstChoiceSection}
+                className="rounded-md w-full font-normal basis-1/2"
+                onChange={(e) => setFirstChoiceSection(e.target.value)}
+              >
+                {allFunctions.map((s) => (
+                  <option
+                    value={s.id}
+                    key={s.id}
+                    disabled={s.name === secondChoiceSection}
+                  >
+                    {s.name}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className="flex flex-col basis-1/2">
+              <p className="font-bold text-sm pb-2">2nd Choice</p>
+              <select
+                value={secondChoiceSection}
+                className="rounded-md w-full font-normal basis-1/2"
+                onChange={(e) => setSecondChoiceSection(e.target.value)}
+              >
+                <option value={''}>None</option>
+                {allFunctions.map((s) => (
+                  <option
+                    value={s.id}
+                    key={s.id}
+                    disabled={s.name === secondChoiceSection}
+                  >
+                    {s.name}
+                  </option>
+                ))}
+              </select>
+            </div>
+          </div>
+        </ProfileSectionHeader>
+        <ProfileSectionHeader title="Add New Section(s)">
+          <>
+            <p className="font-bold text-sm">Select a Section</p>
+            <FunctionSelect
+              allFunctions={allFunctions}
+              selectedFunctions={currentFunctions}
+              onFunctionSelect={setCurrentFunctions}
+            />
+            <p className="font-bold text-sm pt-4">Selected Sections</p>
+            <p className="text-sm">
+              You can remove your choices below to make changes
+            </p>
+            <div className="flex flex-wrap gap-2 py-4">
+              {currentFunctions.map((func) => (
+                <div
+                  key={func.id}
+                  className="flex items-center gap-1 px-2 py-1 text-sm bg-calGreen border border-gray-300 rounded"
+                >
+                  <span className="text-inputGray">{func.name}</span>
+                  <button
+                    type="button"
+                    onClick={(e) => {
+                      e.preventDefault();
+                      setCurrentFunctions(
+                        currentFunctions.filter((f) => f.id !== func.id),
+                      );
+                    }}
+                    className="ml-1 p-0.5 hover:bg-gray-100 rounded"
+                  >
+                    <XMarkIcon className="h-4 w-4 text-gray-500" />
+                  </button>
+                </div>
+              ))}
+            </div>
+          </>
+        </ProfileSectionHeader>
+      </div>
+    </>
+  );
+};
+
+export const MemberProfileEditPreferences = ({
+  bcws,
+  emcr,
+  handleClose,
+  handleSave,
+}: {
+  bcws?: {
+    allRoles: BcwsRoleInterface[];
+    originalRoles: BcwsPersonnelRoleInterface[];
+    sectionChoices: { firstChoiceSection?: Section; secondChoiceSection?: Section };
+  };
+  emcr?: {
+    allFunctions: FunctionType[];
+    originalExperiences: ExperienceInterface[];
+    sectionChoices: {
+      firstChoiceSection?: string;
+      secondChoiceSection?: string;
+      thirdChoiceSection?: string;
+    };
+  };
+  handleClose: () => void;
+  handleSave: (personnel: any) => Promise<void>;
+}) => {
+  const [rolesToSave, setRolesToSave] = useState<RoleChanges>({
+    firstChoiceSection: bcws?.sectionChoices?.firstChoiceSection,
+    secondChoiceSection: bcws?.sectionChoices?.secondChoiceSection,
+    roles: [],
+  });
+  const [functionsToSave, setFunctionsToSave] = useState<FunctionChanges>({
+    firstChoiceSection: undefined,
+    secondChoiceSection: undefined,
+    functions: [],
+  });
+  const onSave = async () => {
+    const personnelUpdate: any = {};
+
+    if (bcws) {
+      const newRoles = rolesToSave.roles.filter(
+        (cr) => !bcws.originalRoles.map((or) => or.id).includes(cr.id),
+      );
+      const rolesExcludingRemoved = bcws.originalRoles
+        .filter((or) => rolesToSave.roles.map((cr) => cr.id).includes(or.id))
+        .map((r) => ({
+          roleId: r.id,
+          expLevel: r.expLevel,
+        }));
+      const updateRoles = [
+        ...newRoles.map((r) => ({
+          roleId: r.id,
+          expLevel: ExperienceLevel.INTERESTED,
+        })),
+        ...rolesExcludingRemoved,
+      ];
+      personnelUpdate.bcws = {
+        firstChoiceSection: rolesToSave.firstChoiceSection,
+        secondChoiceSection: rolesToSave.secondChoiceSection,
+        roles: updateRoles,
+      };
+    }
+
+    if (emcr) {
+      const newFunctions = functionsToSave.functions.filter(
+        (cf) =>
+          !emcr.originalExperiences.map((oe) => oe.function.id).includes(cf.id),
+      );
+      const functionsExcludingRemoved = emcr.originalExperiences
+        .filter((oe) => functionsToSave.functions.map((cf) => cf.id).includes(oe.id))
+        .map((f) => ({
+          id: f.id,
+          function: f.function,
+          experienceType: f.experienceType,
+        }));
+      const updateFunctions = [
+        ...newFunctions.map((f) => ({
+          id: f.id,
+          functionName: f.name,
+          experienceType: Experience.INTERESTED,
+        })),
+        ...functionsExcludingRemoved,
+      ];
+      personnelUpdate.emcr = {
+        // firstChoiceSection,
+        // secondChoiceSection,
+        experiences: updateFunctions,
+      };
+    }
+
+    if (Object.keys(personnelUpdate).length) {
+      handleSave(personnelUpdate);
+    }
+    handleClose();
+  };
+
+  return (
+    <div className="pb-6">
+      {bcws && emcr && (
+        <div className="pt-6 px-12">
+          <h2 className="text-xl font-bold text-gray-900">EMCR</h2>
+        </div>
+      )}
+      {emcr && (
+        <MemberProfileEditFunctions
+          allFunctions={emcr.allFunctions}
+          originalExperiences={emcr.originalExperiences}
+          sectionChoices={emcr.sectionChoices}
+          handleChange={setFunctionsToSave}
+        />
+      )}
+      {bcws && emcr && (
+        <div className="pt-6 px-12 border-t-2 mt-6 border-defaultGray"></div>
+      )}
+      {bcws && (
+        <MemberProfileEditRoles
+          allRoles={bcws.allRoles}
+          originalRoles={bcws.originalRoles}
+          sectionChoices={bcws.sectionChoices}
+          handleChange={setRolesToSave}
+        />
+      )}
       <div className="flex flex-row content-end pt-6 px-6 border-t-4 justify-end gap-2">
         <Button
           variant={ButtonTypes.PRIMARY}
