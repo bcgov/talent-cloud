68e05e6cfddab27058809ddc2685d6393fa31715
Chelsea <chelsea.brown1@ca.ey.com>
2024-12-12 11:42:51 -0800

    redirect at root url to home if no credentials, and unauthorized if credentials + no role (#377)

--- frontend/src/routes/Redirect.tsx
+++ frontend/src/routes/Redirect.tsx
@@ -3,11 +3,17 @@ import { useRoleContext } from '@/providers';
 import { Navigate } from 'react-router-dom';
 import { Routes } from '.';
 import { Loading } from '@/components';
+import { useKeycloak } from '@react-keycloak/web';
 
 const Redirect = () => {
   const { roles, loading } = useRoleContext();
+  const { keycloak } = useKeycloak();
+
   if (loading) {
     return <Loading />;
+  }
+  if (!keycloak.authenticated) {
+    return <Navigate to={Routes.Home} />;
   }
   if (roles?.includes(Role.COORDINATOR) || roles?.includes(Role.LOGISTICS)) {
     return <Navigate to={Routes.Dashboard} />;
@@ -17,6 +23,9 @@ const Redirect = () => {
   }
   if (roles?.includes(Role.MEMBER)) {
     return <Navigate to={Routes.MemberProfile} />;
+  }
+  if (!roles) {
+    return <Navigate to={Routes.Unauthenticated} />;
   }
 };
 
