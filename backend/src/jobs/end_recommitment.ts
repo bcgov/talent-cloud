c01ec27109368a52fb02c16e7b94a7f6dc2fedfe
chelsea-EYDS <chelsea.brown1@ca.ey.com>
2025-01-13 20:06:02 -0800

    FIX - end recommitment on job run,regardless of db end date

--- backend/src/jobs/end_recommitment.ts
+++ backend/src/jobs/end_recommitment.ts
@@ -5,70 +5,51 @@ import { join } from 'path';
 import { AppModule } from '../app.module';
 import { AppLogger } from '../logger/logger.service';
 import { RecommitmentService } from '../recommitment/recommitment.service';
-import { datePST } from '../common/helpers';
+import { datasource } from '../database/datasource';
+import { RecommitmentCycleEntity } from '../database/entities/recommitment/recommitment-cycle.entity';
 
 (async () => {
-  try{
-  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
-    rawBody: true,
-    bufferLogs: true,
-  });
+  try {
+    const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+      rawBody: true,
+      bufferLogs: true,
+    });
 
-  const logger = new AppLogger();
-  app.useLogger(logger);
+    const logger = new AppLogger();
+    app.useLogger(logger);
 
-  app.useStaticAssets(join(__dirname, '..', 'views'));
-  app.setBaseViewsDir(join(__dirname, '..', 'views'));
-  nunjucks.configure(join(__dirname, '..', 'views'), {
-    autoescape: true,
-    throwOnUndefined: false,
-    trimBlocks: false,
-    lstripBlocks: false,
-    watch: true,
-    noCache: process.env.NODE_ENV === 'local' ? true : false,
-    express: app,
-  });
+    app.useStaticAssets(join(__dirname, '..', 'views'));
+    app.setBaseViewsDir(join(__dirname, '..', 'views'));
+    nunjucks.configure(join(__dirname, '..', 'views'), {
+      autoescape: true,
+      throwOnUndefined: false,
+      trimBlocks: false,
+      lstripBlocks: false,
+      watch: true,
+      noCache: process.env.NODE_ENV === 'local' ? true : false,
+      express: app,
+    });
 
-  app.engine('njk', nunjucks.render);
-  app.set('view cache', true);
-  app.setViewEngine('njk');
-  const recommitmentService = app.get(RecommitmentService);
-  const recommitment_cycle =
-    await recommitmentService.checkRecommitmentPeriod();
-  const today = new Date(datePST(new Date()));
+    app.engine('njk', nunjucks.render);
+    app.set('view cache', true);
+    app.setViewEngine('njk');
+    const recommitmentService = app.get(RecommitmentService);
+    const recommitment_cycle =
+      await recommitmentService.checkRecommitmentPeriod();
+    if (!datasource.isInitialized) {
+      await datasource.initialize();
+    }
+    const recommitmentCycleRepository = datasource.getRepository(
+      RecommitmentCycleEntity,
+    );
 
-  if (today.getDate() === recommitment_cycle.endDate.getDate()) {
+    await recommitmentCycleRepository.update(
+      { year: recommitment_cycle.year },
+      { endDate: new Date() },
+    );
+
     if (process.env.ENV !== 'production') {
-      const testEmails = process.env.TEST_EMAIL.split(',');
-
-      if (!testEmails) {
-        logger.error('No test emails found', 'Recommitment');
-        return await app.close();
-      }
-
-      logger.log(testEmails, 'Recommitment');
-
-      const data = await recommitmentService.handleEndRecommitment(
-        true,
-        testEmails,
-      );
-
-      logger.log('Supervisor TEST emails:', 'Recommitment');
-      logger.log(`TxId: ${data?.supervisor?.txId}`, 'Recommitment');
-
-      data.supervisor?.messages?.forEach((supervisor) => {
-        logger.log(`Supervisor: ${supervisor?.to}`, 'Recommitment');
-      });
-
-      logger.log('Member TEST emails:', 'Recommitment');
-      logger.log(`TxId: ${data?.member?.txId}`, 'Recommitment');
-
-      data.member?.messages?.forEach((member) => {
-        logger.log(`Member: ${member?.to}`, 'Recommitment');
-      });
-
-      logger.log('Recommitment job completed', 'Recommitment');
-
+      await testRun(recommitmentService);
       return await app.close();
     } else {
       const data = await recommitmentService.handleEndRecommitment();
@@ -91,15 +72,43 @@ import { datePST } from '../common/helpers';
 
       return await app.close();
     }
-  } else {
-    logger.warn('Not end date for recommitment');
-
-    return await app.close();
+  } catch (e) {
+    console.error(e);
+  } finally {
+    process.exit(0);
   }
-}catch(e){
-  console.error(e);
-}
-finally {
-  process.exit(0);
-}
-})()
+})();
+
+const testRun = async (recommitmentService: RecommitmentService) => {
+  const logger = new AppLogger();
+  const testEmails = process.env.TEST_EMAIL?.split(',');
+
+  if (!testEmails) {
+    logger.error('No test emails found', 'Recommitment');
+
+    return;
+  }
+
+  logger.log(testEmails, 'Recommitment');
+
+  const data = await recommitmentService.handleEndRecommitment(
+    true,
+    testEmails,
+  );
+
+  logger.log('Supervisor TEST emails:', 'Recommitment');
+  logger.log(`TxId: ${data?.supervisor?.txId}`, 'Recommitment');
+
+  data.supervisor?.messages?.forEach((supervisor) => {
+    logger.log(`Supervisor: ${supervisor?.to}`, 'Recommitment');
+  });
+
+  logger.log('Member TEST emails:', 'Recommitment');
+  logger.log(`TxId: ${data?.member?.txId}`, 'Recommitment');
+
+  data.member?.messages?.forEach((member) => {
+    logger.log(`Member: ${member?.to}`, 'Recommitment');
+  });
+
+  logger.log('Recommitment job completed', 'Recommitment');
+};
