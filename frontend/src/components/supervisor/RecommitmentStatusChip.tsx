1380682700b9edfa18dfff855298ff275de49403
Chelsea <chelsea.brown1@ca.ey.com>
2025-01-31 10:43:20 -0800

    supervisor table clean up (#493)

--- frontend/src/components/supervisor/RecommitmentStatusChip.tsx
+++ frontend/src/components/supervisor/RecommitmentStatusChip.tsx
@@ -2,6 +2,7 @@ import {
   RecommitmentStatus,
   RecommitmentStatusLabel,
 } from '@/common/enums/recommitment-status';
+import { Tooltip } from '@/components/ui/Tooltip';
 
 export const RecommitmentStatusChip = ({
   status,
@@ -25,15 +26,33 @@ export const RecommitmentStatusChip = ({
         return 'text-gray-600';
     }
   };
-
+const getContent = (status: RecommitmentStatus|string) => {
+  switch (status) {
+    case RecommitmentStatus.PENDING:
+      return 'Pending member recommitment. No action required at this time.';
+    case RecommitmentStatus.MEMBER_COMMITTED:
+      return 'Member recommitted. Supervisor approval required.';
+    case RecommitmentStatus.SUPERVISOR_APPROVED:
+      return 'Member recommitted and approved for the upcoming year.';
+    case RecommitmentStatus.MEMBER_DENIED:
+    case RecommitmentStatus.MEMBER_NO_RESPONSE:
+    case RecommitmentStatus.SUPERVISOR_DENIED:
+    case RecommitmentStatus.SUPERVISOR_NO_RESPONSE:
+      return 'Member not returning in the upcoming year.';
+    default:
+      return 'text-gray-600';
+  }
+}
   return (
+    <Tooltip  content={getContent(status)} placement={'bottom'}>
     <div
       className={
-        'px-4 py-1.5 rounded-full text-sm flex flex-row justify-center text-center ' +
+        'px-4 py-1.5 rounded-full text-sm flex flex-row justify-center text-center lg:min-w-[150px] ' +
         getClassName(status)
       }
     >
       {RecommitmentStatusLabel[status as keyof typeof RecommitmentStatus] ?? 'N/A'}
     </div>
+    </Tooltip>
   );
 };
