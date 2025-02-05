693c7919fde6a1fd9462661d0653ac6c155f6cac
chelsea-EYDS <chelsea.brown1@ca.ey.com>
2025-02-05 12:49:16 -0800

    fix layout shift

--- frontend/src/components/layout/private/Layout.tsx
+++ frontend/src/components/layout/private/Layout.tsx
@@ -6,11 +6,11 @@ export const Layout = ({ children }: { children: ReactElement }) => {
   return (
     <div className="h-screen overflow-y-hidden">
       <Header />
+
       <div className="pt-8 md:pt-32 lg:pt-16 w-full  h-full  bg-white overflow-y-auto">
-        {children}
+        <div className="h-auto lg:min-h-[1200px] min-h-[1000px]">{children}</div>
         <Footer />
       </div>
-      
     </div>
   );
 };
