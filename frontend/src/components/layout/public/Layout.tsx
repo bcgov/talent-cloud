e452d759dd8d44bec46c85d2750d67564fd7826e
Chelsea <chelsea.brown1@ca.ey.com>
2025-02-05 20:45:43 -0800

    fix layout shift (#501)

--- frontend/src/components/layout/public/Layout.tsx
+++ frontend/src/components/layout/public/Layout.tsx
@@ -6,11 +6,14 @@ import { footer, landAcknowledgement } from '../constants';
 
 export const PublicLayout = ({ children }: { children: ReactElement }) => {
   return (
-    <div className="bg-white h-screen">
+    <div className="bg-white h-screen overflow-y-hidden">
       <Header />
-      {children}
-      <Banner content={landAcknowledgement} />
-      <Footer content={footer} />
+      <div className="h-full overflow-y-auto">
+        <div className="h-auto lg:min-h-[1200px] min-h-[1000px]">{children}</div>
+
+        <Banner content={landAcknowledgement} />
+        <Footer content={footer} />
+      </div>
     </div>
   );
 };
