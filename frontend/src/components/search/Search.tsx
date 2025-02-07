cb012d3fe93fbe1e72f44b77f07bab08de05f95f
chelsea-EYDS <chelsea.brown1@ca.ey.com>
2025-02-06 16:39:59 -0800

    DRAFT

--- frontend/src/components/search/Search.tsx
+++ frontend/src/components/search/Search.tsx
@@ -1,6 +1,8 @@
 import { MagnifyingGlassIcon } from '@heroicons/react/24/solid';
 import type { FieldInterface } from '..';
 import { classes } from '../filters/classes';
+import { useCallback, useMemo, useState } from 'react';
+import { debounce } from 'lodash';
 import { Filters } from '@/common';
 
 export const Search = ({
@@ -12,15 +14,27 @@ export const Search = ({
   setSearchParams: (searchParams: any) => any;
   field: FieldInterface;
 }) => {
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.value === '') {
+  const [searchValue, setSearchValue] = useState(
+    searchParams.get(Filters.NAME) || '',
+  );
+
+  const sendRequest = useCallback((value: string) => {
+    if (value === '') {
       searchParams.delete(Filters.NAME);
       setSearchParams({ ...Object.fromEntries(searchParams) });
     } else {
-      setSearchParams({ ...Object.fromEntries(searchParams), name: e.target.value });
+      setSearchParams({ ...Object.fromEntries(searchParams), name: value });
     }
-  };
+  }, []);
 
+  const debouncedSearch = useMemo(() => {
+    return debounce(sendRequest, 500);
+  }, [sendRequest]);
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(e.target.value);
+    debouncedSearch(e.target.value);
+  };
   return (
     <div className="relative w-full">
       <label>
@@ -30,7 +44,7 @@ export const Search = ({
           <input
             id={field.name}
             autoComplete="name"
-            value={searchParams.get(Filters.NAME) ?? ''}
+            value={searchValue}
             type="text"
             className={classes.menu.container}
             name={field.name}
