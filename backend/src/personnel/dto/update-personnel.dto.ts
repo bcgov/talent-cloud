673d0cf132192a2da7ff44540838c269dd01f12d
chelsea-EYDS <chelsea.brown1@ca.ey.com>
2025-02-12 14:59:00 -0800

    DTO re-org

--- backend/src/personnel/dto/update-personnel.dto.ts
+++ backend/src/personnel/dto/update-personnel.dto.ts
@@ -1,8 +1,8 @@
 import { ApiProperty, IntersectionType } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import { IsOptional, ValidateIf } from 'class-validator';
-import { UpdateBcwsPersonnelDTO } from 'src/bcws/dto/update-bcws-personnel.dto';
-import { UpdateEmcrPersonnelDTO } from 'src/emcr/dto';
+import { UpdateBcwsPersonnelDTO } from '../../bcws/dto/update-bcws-personnel.dto';
+import { UpdateEmcrPersonnelDTO } from '../../emcr/dto';
 import { UpdatePersonnelDetailsDTO } from './details/update-personnel-details.dto';
 import { UpdateEmergencyContactInfo } from './emergency-contact/update-emergency-contact';
 import { UpdateEmploymentInfo } from './employment-info/update-employment-info.dto';
