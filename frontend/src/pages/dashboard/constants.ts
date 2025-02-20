8208b082207859bc6114af046a7f7d6bbfe7cf92
Leon <leon.leontenko@ca.ey.com>
2025-02-20 11:17:14 -0800

    [TC-993][TC-1066] Change Member Status Guide in the Coordinators' Dashboard (#521)
    * fix: remove unused statusDescriptions
    
    * fix: change CORE to CORE Team

--- frontend/src/pages/dashboard/constants.ts
+++ frontend/src/pages/dashboard/constants.ts
@@ -5,17 +5,17 @@ export const statusDescriptions = {
       {
         title: 'Active',
         description:
-          'Members who are actively participating in CORE; deployable for the current year.',
+          'Members who are actively participating in CORE Team; deployable for the current year.',
       },
       {
         title: 'Inactive',
         description:
-          'Members who are currently not participating in CORE; not deployable for the current year. Consists of both “temporarily inactive” (e.g., due to annual leave, maternity leave, short-term hiatus from the program, etc.) and “permanently inactive” (e.g., retired, removed from the program, etc.) members. *Permanently inactive members are greyed out.',
+          'Members who are currently not participating in CORE Team; not deployable for the current year. Consists of both “temporarily inactive” (e.g., due to annual leave, maternity leave, short-term hiatus from the program, etc.) and “permanently inactive” (e.g., retired, removed from the program, etc.) members. *Permanently inactive members are greyed out.',
       },
       {
         title: 'Pending Approval',
         description:
-          'CORE applicants whose application is currently under review by program staff and has not yet been approved for membership.',
+          'CORE Team applicants whose application is currently under review by program staff and has not yet been approved for membership.',
       },
     ],
   },
@@ -25,31 +25,19 @@ export const statusDescriptions = {
       {
         title: '"NEW"',
         type: 'Active',
-        description: 'New members approved into CORE within the last 5 days.',
-      },
-      {
-        title: '"ACTIVE ONLY"',
-        type: 'Active',
-        description:
-          'Members who are active CORE participants for the current year but with pending recommitment.',
+        description: 'New members approved into CORE Team, the tag will last for 5 days.',
       },
       {
         title: '"RECOMMITTED"',
         type: 'Active',
         description:
-          'Members who are active CORE participants for the current year AND will also be returning as active members for the upcoming year. This label will disappear when the recommitment period ends.',
+          'Members who are active CORE Team participants for the current year AND will also be returning as active members for the upcoming year. This label will disappear when the recommitment period ends.',
       },
       {
         title: '"MISSED DEADLINE"',
         type: 'Inactive',
         description:
           'Members who turned inactive due to missing the recommitment deadline.',
-      },
-      {
-        title: '"REACTIVATED"',
-        type: 'Inactive',
-        description:
-          'Members who turned inactive due to missing the recommitment deadline, but has their recommitment process temporarily re-initiated beyond the recommitment period.',
       },
       {
         title: '"NOT RETURNING"',
