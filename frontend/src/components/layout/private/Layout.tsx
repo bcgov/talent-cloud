1aa62657e00a3db3d10c0e3f9c613dd0da7fb472
chelsea-EYDS <chelsea.brown1@ca.ey.com>
2025-02-27 15:04:50 -0800

    stepper

--- frontend/src/components/layout/private/Layout.tsx
+++ frontend/src/components/layout/private/Layout.tsx
@@ -4,11 +4,10 @@ import { Header } from './Header';
 
 export const Layout = ({ children }: { children: ReactElement }) => {
   return (
-    <div className="h-screen overflow-y-hidden">
+    <div className="h-screen overflow-y-hidden flex flex-col justify-between">
       <Header />
-
-      <div className="pt-8 md:pt-32 lg:pt-16 w-full  h-full  bg-white overflow-y-auto">
-        <div className="h-auto lg:min-h-[1200px] min-h-[1000px]">{children}</div>
+      <div className="w-full h-full  pt-20 bg-white overflow-y-auto">
+        {children}
         <Footer />
       </div>
     </div>
--- frontend/src/pages/intake-form/IntakeForm.tsx
+++ frontend/src/pages/intake-form/IntakeForm.tsx
@@ -1,44 +1,35 @@
 import { Form, Formik } from 'formik';
-import { ProgramSelection } from './pages/ProgramSelection';
-import { PersonalInfo } from './pages/PersonalInfo';
-import { ExperienceRoles } from './pages/ExperienceRoles';
-import { Skills } from './pages/Skills';
-import { ReviewAndSubmit } from './pages/ReviewAndSubmit';
-import { Complete } from './pages/Complete';
 import { personalDetailsSchema, programSelectionSchema } from './validation';
 import { personalDetails, programFields } from './fields';
-import { useRoleContext } from '@/providers';
 import { useKeycloak } from '@react-keycloak/web';
-import {
-  experienceRolesSchema,
-  personalInfoSchema,
-  programSelectionSchema,
-  skillsSchema,
-} from './validation';
-import { personalInfoFields, programFields } from './fields';
-import { useKeycloak } from '@react-keycloak/web';
-        
-const IntakeForm = () => {
 
+import { Tab, TabGroup, TabList, TabPanel, TabPanels } from '@headlessui/react';
+import { ButtonTypes } from '@/common';
+import { Button } from '@/components/ui';
+import clsx from 'clsx';
+import { useState } from 'react';
+import { formTabs } from './tabs';
+
+const IntakeForm = () => {
   const { keycloak } = useKeycloak();
   const { tokenParsed } = keycloak;
+  const [selectedTab, setSelectedTab] = useState(0);
 
   if (!tokenParsed) {
     return;
   }
 
+  const handleSelectTab = (value: any) => {
+    setSelectedTab(value);
+  };
+
   return (
-    <div className="pt-24 px-24">
+    <div className="h-full flex flex-col justify-between">
       <Formik
         initialValues={{
           programFields,
-
           personalDetails: {
             ...personalDetails,
-
-          personalInfoFields: {
-            ...personalInfoFields,
-
             firstName: tokenParsed.given_name,
             lastName: tokenParsed.family_name,
           },
@@ -47,8 +38,6 @@ const IntakeForm = () => {
         validationSchema={{
           programSelectionSchema,
           personalDetailsSchema,
-          experienceRolesSchema,
-          skillsSchema,
         }}
         onSubmit={(values, actions) => {
           // TODO: Update
@@ -59,16 +48,95 @@ const IntakeForm = () => {
         }}
       >
         <Form>
-          <h1>Intake Form</h1>
-          {/* no need to pass props - access values and validation via useFormikContext() hook */}
-          <ProgramSelection />
-          <PersonalInfo />
-          <ExperienceRoles />
-          <Skills />
-          <ReviewAndSubmit />
-          <Complete />
+          <TabGroup
+            vertical
+            manual
+            selectedIndex={selectedTab}
+            onChange={setSelectedTab}
+            className="flex flex-row space-x-24 xl:space-x-32 px-16 lg:px-24 xl:px-32 w-full pt-24"
+          >
+            <TabList className="flex flex-col space-y-16 mt-2  border-dashed border-blue-900 border-l">
+              {formTabs.map(({ label, value }, index) => (
+                // {/* <Tab className="data-[selected]:bg-blue-500 data-[selected]:text-white data-[hover]:underline"> */}
+                <Tab
+                  key={value}
+                  value={value}
+                  onClick={() => handleSelectTab(value)}
+                  className={'data-[selected]:outline-none'}
+                >
+                  {({ selected }) => (
+                    <div className="flex flex-row space-x-2 flex-nowrap text-nowrap h-full">
+                      <div
+                        className={clsx(
+                          selected &&
+                            'bg-blue-800 text-white border-2 border-blue-800',
+                          !selected && ' border-2 border-[#606060] bg-white',
+                          ' px-2 -ml-3 rounded-full ',
+                        )}
+                      >
+                        {index + 1}
+                      </div>
+                      <div>
+                        <p
+                          className={clsx(
+                            selected && 'outline-none text-blue-800 font-bold',
+                            'text-sm  text-[#606060]',
+                          )}
+                        >
+                          {label}
+                        </p>
+                      </div>
+                    </div>
+                  )}
+                </Tab>
+              ))}
+            </TabList>
+            <TabPanels>
+              {formTabs.map(({ value, children, label, description }) => (
+                <TabPanel key={value}>
+                  <div className="h-full flex flex-col xl:pr-24">
+                    <h3>{label}</h3>
+                    <div className="text-sm py-6">{description}</div>
+
+                    {children}
+                  </div>
+                </TabPanel>
+              ))}
+            </TabPanels>
+          </TabGroup>
         </Form>
       </Formik>
+      <div>
+        <div className="border border-t-grey-200"></div>
+        <div className="flex flex-row justify-between py-8 px-32">
+          <div className="flex flex-row space-x-6">
+            <Button
+              text="Cancel"
+              variant={ButtonTypes.TEXT}
+              onClick={() => console.log('clicked')}
+            />
+            <Button
+              text="Save For Later"
+              variant={ButtonTypes.OUTLINED}
+              onClick={() => console.log('clicked')}
+            />
+          </div>
+          <div className="flex flex-row space-x-6">
+            <Button
+              text="Previous"
+              disabled={selectedTab === 0}
+              variant={ButtonTypes.OUTLINED}
+              onClick={() => setSelectedTab(selectedTab - (1 % formTabs.length))}
+            />
+            <Button
+              text="Next"
+              variant={ButtonTypes.SOLID}
+              disabled={selectedTab === formTabs.length - 1}
+              onClick={() => setSelectedTab(selectedTab + (1 % formTabs.length))}
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
