f33089740caa79601d2b4fcbff7b029500f4130c
chelsea-EYDS <chelsea.brown1@ca.ey.com>
2025-02-28 10:59:44 -0800

    stepper

--- frontend/src/pages/intake-form/pages/Complete.tsx
+++
@@ -1,9 +0,0 @@
-import { useFormikContext } from 'formik';
-
-import type { FormSection } from '../types';
-
-export const Complete = ({ sections }: { sections: FormSection[] }) => {
-  const { values, handleChange } = useFormikContext();
-  console.log(values, handleChange, sections);
-  return <></>;
-};
--- frontend/src/pages/intake-form/pages/ReviewAndSubmit.tsx
+++
@@ -1,9 +0,0 @@
-import { useFormikContext } from 'formik';
-
-import type { FormSection } from '../types';
-
-export const ReviewAndSubmit = ({ sections }: { sections: FormSection[] }) => {
-  const { values, handleChange } = useFormikContext();
-  console.log(values, handleChange, sections);
-  return <></>;
-};
