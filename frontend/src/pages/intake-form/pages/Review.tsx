1f47647e44dcf3e464af82df49be89a68e19482b
chelsea-EYDS <chelsea.brown1@ca.ey.com>
2025-03-25 00:08:55 -0700

    UI bugs

--- frontend/src/pages/intake-form/pages/Review.tsx
+++ frontend/src/pages/intake-form/pages/Review.tsx
@@ -12,7 +12,11 @@ import clsx from 'clsx';
 import { Fragment } from 'react';
 import { DriverLicenseName, MinistryName, Program } from '@/common';
 import { BcwsRoleName } from '@/common/enums/sections.enum';
-import { expectationsBcws, expectationsBoth, expectationsEmcr } from '../constants/enums';
+import {
+  expectationsBcws,
+  expectationsBoth,
+  expectationsEmcr,
+} from '../constants/enums';
 import { TravelPreferenceText } from '@/common/enums/travel-preference.enum';
 import { ToolsProficiencyName } from '@/common/enums/tools.enum';
 
@@ -26,11 +30,10 @@ const ReviewFields = ({
   const { values } = useFormikContext<IntakeFormValues>();
 
   const getValue = (value: any, name: string) => {
-
-    if(value === 'true') return 'Yes'
-    if (value === 'false') return "No"
-    if (!value || value === "" || value.length === 0 ) return '--'
-    switch (name) {        
+    if (value === 'true') return 'Yes';
+    if (value === 'false') return 'No';
+    if (!value || value === '' || value.length === 0) return '--';
+    switch (name) {
       case 'homeLocation':
         return value?.name ?? '--';
       case 'functions':
@@ -45,10 +48,14 @@ const ReviewFields = ({
       case 'expiry':
         return value && format(value, 'yyyy-MM-dd');
       case 'toolProficiency':
-        return value ? ToolsProficiencyName[value as keyof typeof ToolsProficiencyName] : '--';
+        return value
+          ? ToolsProficiencyName[value as keyof typeof ToolsProficiencyName]
+          : '--';
       case 'travelPreferenceEmcr':
       case 'travelPreferenceBcws':
-        return value && TravelPreferenceText[value as keyof typeof TravelPreferenceText];
+        return (
+          value && TravelPreferenceText[value as keyof typeof TravelPreferenceText]
+        );
       case 'tool':
         return (value && value.name) || '--';
       case 'firstChoiceFunction':
@@ -115,8 +122,15 @@ const ReviewFields = ({
         .map((field: any, index: number) => (
           <Fragment key={field.name + index.toString()}>
             {!field.nestedFields ? (
-              <div key={field.name} className={clsx(field.colSpan ? `col-span-${field.colSpan}` : 'col-span-1')}>
-                {field.label && <div className="subtext text-sm pb-2">{field.label}</div>}
+              <div
+                key={field.name}
+                className={clsx(
+                  field.colSpan ? `col-span-${field.colSpan}` : 'col-span-1',
+                )}
+              >
+                {field.label && (
+                  <div className="subtext text-sm pb-2">{field.label}</div>
+                )}
                 {field.helperText && (
                   <div className="subtext text-sm pb-2">{field.helperText}</div>
                 )}
@@ -139,7 +153,9 @@ const ReviewFields = ({
                             className="col-span-1 flex flex-col"
                             key={innerField.name}
                           >
-                            <div className="subtext text-sm pb-2">{innerField.label}</div>
+                            <div className="subtext text-sm pb-2">
+                              {innerField.label}
+                            </div>
                             <div className="text-[#262729] ">
                               {getValue(
                                 itm?.[innerField.name as keyof typeof itm] ?? '',
@@ -198,21 +214,47 @@ export const Review = ({ sections }: { sections: FormSectionType[] }) => {
         <>
           <div className={`col-span-2`}>
             <div className="subtext text-sm">Program</div>
-            <div className="text-[#262729] ">{values.program && values.program === Program.ALL ?  "EMCR & BCWS" : values.program && values.program.toUpperCase()
-            
-        }</div>
+            <div className="text-[#262729] ">
+              {values.program && values.program === Program.ALL
+                ? 'EMCR & BCWS'
+                : values.program && values.program.toUpperCase()}
+            </div>
           </div>
           <div className={`col-span-2`}>
             <div className="subtext text-sm pb-4">Acknowledgement</div>
             <div className="flex flex-col gap-y-4">
-              
+              {values.acknowledgement &&
+                values.program === Program.EMCR &&
+                expectationsEmcr.map((itm, index) => (
+                  <div
+                    className="text-[#262729] text-sm"
+                    key={index.toString() + 'ack'}
+                  >
+                    {itm.label}
+                  </div>
+                ))}
 
-{values.acknowledgement && values.program === Program.EMCR && expectationsEmcr.map((itm, index) => <div className="text-[#262729] text-sm" key={index.toString() + 'ack'}>{itm.label}</div>) }
-          
-{values.acknowledgement && (values.program === Program.ALL) && expectationsBoth.map((itm, index) => <div className="text-[#262729] text-sm" key={index.toString() + 'ack'}>{itm.label}</div>)}
+              {values.acknowledgement &&
+                values.program === Program.ALL &&
+                expectationsBoth.map((itm, index) => (
+                  <div
+                    className="text-[#262729] text-sm"
+                    key={index.toString() + 'ack'}
+                  >
+                    {itm.label}
+                  </div>
+                ))}
 
-{values.acknowledgement && (values.program === Program.BCWS) && expectationsBcws.map((itm, index) => <div className="text-[#262729] text-sm" key={index.toString() + 'ack'}>{itm.label}</div>)}
-
+              {values.acknowledgement &&
+                values.program === Program.BCWS &&
+                expectationsBcws.map((itm, index) => (
+                  <div
+                    className="text-[#262729] text-sm"
+                    key={index.toString() + 'ack'}
+                  >
+                    {itm.label}
+                  </div>
+                ))}
             </div>
           </div>
         </>
