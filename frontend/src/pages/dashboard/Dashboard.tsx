8976095202c89e21b0649a02c7acc3e884f87b93
Chelsea <chelsea.brown1@ca.ey.com>
2025-03-28 19:10:27 -0700

    [CHORE] reverting download button remove (#647)
    * reverting download button remove
    
    * reverting download button remove

--- frontend/src/pages/dashboard/Dashboard.tsx
+++ frontend/src/pages/dashboard/Dashboard.tsx
@@ -27,12 +27,15 @@ import { button as buttonClass } from '@/components/ui/classes';
 
 // icons
 import { QuestionMarkCircleIcon } from '@heroicons/react/24/solid';
+import { useExportToCSV } from '@/hooks/useExportToCSV';
 
 const Dashboard = () => {
   const { recommitmentCycle, isRecommitmentCycleOpen } = useRecommitmentCycle();
   const [showDescriptionsModal, setShowDescriptionsModal] = useState(false);
   const { program, roles } = useRoleContext();
-  
+  const { csvExport } = useExportToCSV();
+  const [ dlButtonText, setDlButtonText ] = useState('Downloading All Members');
+  const [ dlDisabled, setDlDisabled ] = useState(false);
 
   const {
     totalRows,
@@ -85,10 +88,28 @@ const Dashboard = () => {
                   }}
                 />
               )}
-              {/* {roles && roles.includes(Role.COORDINATOR) && (
+              {roles && roles.includes(Role.COORDINATOR) && (
                 <button
                   disabled={dlDisabled}
-                  onClick={download
+                  onClick={async () => {
+                    //Disable button while download method runs
+                    setDlDisabled(true);
+                    setDlButtonText('Downloading...')
+
+                    //download file from export hook according to coordinator's program and name appropriately
+                    const csvReceipt = await csvExport(program);
+                    const url = window.URL.createObjectURL(new Blob([csvReceipt]));
+                    const link = document.createElement('a');
+                    link.href = url;
+                    link.download = program?.toString().toUpperCase() + '_Personnel_Details.csv';
+                    document.body.appendChild(link);
+                    link.click();
+                    link.remove();
+
+                    //Re-enable button after download is complete
+                    setDlButtonText('Download All Members');
+                    setDlDisabled(false);
+                  }
                 } 
                   className={buttonClass.tertiaryButton}
                 > 
@@ -97,7 +118,7 @@ const Dashboard = () => {
                     {' '}
                   </span>
                 </button>
-              )} */}
+              )}
               {roles && roles.includes(Role.COORDINATOR) && (
                 <button
                   onClick={() => setShowDescriptionsModal(!showDescriptionsModal)}
