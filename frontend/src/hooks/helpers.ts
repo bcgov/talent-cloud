5963636c2029a2b0241f5e60d0de1903bf6b51bd
Fintan Roche <Fintan.Roche@quartech.com>
2025-09-04 14:33:48 -0700

    CORE EMCR - User Days of Availability bug fix

--- frontend/src/hooks/helpers.ts
+++ frontend/src/hooks/helpers.ts
@@ -14,199 +14,196 @@ import type { DateRange } from 'react-day-picker';
  * @returns
  */
 export const getAvailabilityValue = (
-  availabilityType: AvailabilityType,
-  availabilityDates: DateRange,
-  availability: Availability[],
+    availabilityType: AvailabilityType,
+    availabilityDates: DateRange,
+    availability: Availability[],
 ) => {
-  const totalAvailableDays =
-    availabilityType === AvailabilityType.AVAILABLE
-      ? availability.filter((itm) => itm.availabilityType !== availabilityType)
-          .length
-      : availability.filter((itm) => itm.availabilityType === availabilityType)
-          .length;
-  const totalDaysSearched =
-    differenceInDays(
-      availabilityDates.to ?? new Date(),
-      availabilityDates.from ?? new Date(),
-    ) + 1;
+    const totalDaysOfAvailabilityType =
+        availability.filter((itm) => itm.availabilityType === availabilityType)
+            .length;
+    const totalDaysSearched =
+        differenceInDays(
+            availabilityDates.to ?? new Date(),
+            availabilityDates.from ?? new Date(),
+        ) + 1;
 
-  if (totalDaysSearched === 1 || totalDaysSearched === totalAvailableDays) {
+    if (totalDaysSearched === 1 || totalDaysSearched === totalDaysOfAvailabilityType) {
+        return {
+            availability:
+                AvailabilityTypeName[availabilityType as keyof typeof AvailabilityType],
+        };
+    }
+
+    if (totalDaysSearched > 1 && totalDaysOfAvailabilityType >= 1) {
+        return {
+            availability: AvailabilityTypeName[availabilityType],
+            days: `${totalDaysOfAvailabilityType} of ${totalDaysSearched} days`,
+        };
+    }
+
     return {
-      availability:
-        AvailabilityTypeName[availabilityType as keyof typeof AvailabilityType],
+        availability:
+            AvailabilityTypeName[availabilityType as keyof typeof AvailabilityType],
     };
-  }
-
-  if (totalDaysSearched > 1 && totalAvailableDays >= 1) {
-    return {
-      availability: AvailabilityTypeName[availabilityType],
-      days: `${totalAvailableDays} of ${totalDaysSearched} days`,
-    };
-  }
-
-  return {
-    availability:
-      AvailabilityTypeName[availabilityType as keyof typeof AvailabilityType],
-  };
 };
 
 const renderAvailability = (personnel: Personnel, filterValues: URLSearchParams) => {
-  if (
-    filterValues?.get('availabilityType') &&
-    personnel?.availability &&
-    personnel?.availability?.length > 0
-  ) {
-    return getAvailabilityValue(
-      filterValues.get('availabilityType') as AvailabilityType,
-      {
-        from: new Date(filterValues.get('availabilityFromDate') ?? ''),
-        to: new Date(filterValues.get('availabilityToDate') ?? ''),
-      },
-      personnel.availability,
-    );
-  } else if (!personnel.availability) {
-    return { availability: '' };
-  } else if (personnel.availability.length > 0) {
-    return {
-      availability:
-        AvailabilityTypeName[
-          personnel.availability[0].availabilityType as keyof typeof AvailabilityType
-        ],
-    };
-  }
+    if (
+        filterValues?.get('availabilityType') &&
+        personnel?.availability &&
+        personnel?.availability?.length > 0
+    ) {
+        return getAvailabilityValue(
+            filterValues.get('availabilityType') as AvailabilityType,
+            {
+                from: new Date(filterValues.get('availabilityFromDate') ?? ''),
+                to: new Date(filterValues.get('availabilityToDate') ?? ''),
+            },
+            personnel.availability,
+        );
+    } else if (!personnel.availability) {
+        return { availability: '' };
+    } else if (personnel.availability.length > 0) {
+        return {
+            availability:
+                AvailabilityTypeName[
+                personnel.availability[0].availabilityType as keyof typeof AvailabilityType
+                ],
+        };
+    }
 };
 
 export const renderCells = (
-  personnel: Personnel,
-  searchParms: URLSearchParams,
-  isPending: boolean,
-  program: Program,
+    personnel: Personnel,
+    searchParms: URLSearchParams,
+    isPending: boolean,
+    program: Program,
 ) => {
-  const cells = {
-    name: {
-      key: DashboardColumns.NAME,
-      columnName: DashboardColumns.NAME,
-      value: `${personnel.lastName?.toUpperCase()},  ${personnel.firstName}`,
-    },
+    const cells = {
+        name: {
+            key: DashboardColumns.NAME,
+            columnName: DashboardColumns.NAME,
+            value: `${personnel.lastName?.toUpperCase()},  ${personnel.firstName}`,
+        },
 
-    region: {
-      key: DashboardColumns.REGION,
-      columnName: DashboardColumns.REGION,
-      value: personnel.homeLocation?.region,
-    },
+        region: {
+            key: DashboardColumns.REGION,
+            columnName: DashboardColumns.REGION,
+            value: personnel.homeLocation?.region,
+        },
 
-    fireCentre: {
-      key: DashboardColumns.FIRE_CENTRE,
-      columnName: DashboardColumns.FIRE_CENTRE,
-      value:
-        FireCentreName[
-          personnel?.homeLocation?.fireCentre as keyof typeof FireCentreName
+        fireCentre: {
+            key: DashboardColumns.FIRE_CENTRE,
+            columnName: DashboardColumns.FIRE_CENTRE,
+            value:
+                FireCentreName[
+                personnel?.homeLocation?.fireCentre as keyof typeof FireCentreName
+                ],
+        },
+        location: {
+            key: DashboardColumns.LOCATION,
+            columnName: DashboardColumns.LOCATION,
+            value: personnel?.homeLocation?.locationName,
+        },
+        ics: {
+            key: DashboardColumns.ICS,
+            columnName: DashboardColumns.ICS,
+            value: personnel?.icsTraining,
+        },
+        supervisorApproval: {
+            key: DashboardColumns.SUPERVISOR_APPROVAL,
+            columnName: DashboardColumns.SUPERVISOR_APPROVAL,
+            value: personnel?.approvedBySupervisor ? 'Yes' : 'No',
+        },
+        unionMembership: {
+            key: DashboardColumns.UNION_MEMBERSHIP,
+            columnName: DashboardColumns.UNION_MEMBERSHIP,
+            value: personnel?.unionMembership,
+        },
+        ministry: {
+            key: DashboardColumns.MINISTRY,
+            columnName: DashboardColumns.MINISTRY,
+            value: personnel.ministry,
+        },
+        availability: {
+            key: DashboardColumns.AVAILABILITY,
+            columnName: DashboardColumns.AVAILABILITY,
+            // value will be the status type and/or the number of days available
+            value: renderAvailability(personnel, searchParms),
+        },
+        travelPreference: {
+            key: DashboardColumns.TRAVEL_PREFERENCE,
+            columnName: DashboardColumns.TRAVEL_PREFERENCE,
+            value: personnel.travelPreference,
+        },
+        dateApplied: {
+            key: DashboardColumns.DATE_APPLIED,
+            columnName: DashboardColumns.DATE_APPLIED,
+            value: personnel.dateApplied && datePST(personnel.dateApplied as Date, true),
+        },
+        dateApproved: {
+            key: DashboardColumns.DATE_APPROVED,
+            columnName: DashboardColumns.DATE_APPROVED,
+            value: personnel.dateApproved && datePST(personnel.dateApproved as Date, true),
+        },
+        parQ: {
+            key: DashboardColumns.PARQ,
+            columnName: DashboardColumns.PARQ,
+            value: personnel.parQ,
+        },
+        orientation: {
+            key: DashboardColumns.ORIENTATION,
+            columnName: DashboardColumns.ORIENTATION,
+            value: personnel.orientation,
+        },
+    };
+
+    const activeCells = {
+        [Program.EMCR]: [
+            cells.name,
+            cells.dateApproved,
+            cells.region,
+            cells.location,
+            cells.availability,
+            cells.travelPreference,
+            cells.unionMembership,
+            cells.ministry,
         ],
-    },
-    location: {
-      key: DashboardColumns.LOCATION,
-      columnName: DashboardColumns.LOCATION,
-      value: personnel?.homeLocation?.locationName,
-    },
-    ics: {
-      key: DashboardColumns.ICS,
-      columnName: DashboardColumns.ICS,
-      value: personnel?.icsTraining,
-    },
-    supervisorApproval: {
-      key: DashboardColumns.SUPERVISOR_APPROVAL,
-      columnName: DashboardColumns.SUPERVISOR_APPROVAL,
-      value: personnel?.approvedBySupervisor ? 'Yes' : 'No',
-    },
-    unionMembership: {
-      key: DashboardColumns.UNION_MEMBERSHIP,
-      columnName: DashboardColumns.UNION_MEMBERSHIP,
-      value: personnel?.unionMembership,
-    },
-    ministry: {
-      key: DashboardColumns.MINISTRY,
-      columnName: DashboardColumns.MINISTRY,
-      value: personnel.ministry,
-    },
-    availability: {
-      key: DashboardColumns.AVAILABILITY,
-      columnName: DashboardColumns.AVAILABILITY,
-      // value will be the status type and/or the number of days available
-      value: renderAvailability(personnel, searchParms),
-    },
-    travelPreference: {
-      key: DashboardColumns.TRAVEL_PREFERENCE,
-      columnName: DashboardColumns.TRAVEL_PREFERENCE,
-      value: personnel.travelPreference,
-    },
-    dateApplied: {
-      key: DashboardColumns.DATE_APPLIED,
-      columnName: DashboardColumns.DATE_APPLIED,
-      value: personnel.dateApplied && datePST(personnel.dateApplied as Date, true),
-    },
-    dateApproved: {
-      key: DashboardColumns.DATE_APPROVED,
-      columnName: DashboardColumns.DATE_APPROVED,
-      value: personnel.dateApproved && datePST(personnel.dateApproved as Date, true),
-    },
-    parQ: {
-      key: DashboardColumns.PARQ,
-      columnName: DashboardColumns.PARQ,
-      value: personnel.parQ,
-    },
-    orientation: {
-      key: DashboardColumns.ORIENTATION,
-      columnName: DashboardColumns.ORIENTATION,
-      value: personnel.orientation,
-    },
-  };
+        [Program.BCWS]: [
+            cells.name,
+            cells.fireCentre,
+            cells.location,
+            cells.availability,
+            cells.travelPreference,
+            cells.unionMembership,
+            cells.ministry,
+        ],
+    };
 
-  const activeCells = {
-    [Program.EMCR]: [
-      cells.name,
-      cells.dateApproved,
-      cells.region,
-      cells.location,
-      cells.availability,
-      cells.travelPreference,
-      cells.unionMembership,
-      cells.ministry,
-    ],
-    [Program.BCWS]: [
-      cells.name,
-      cells.fireCentre,
-      cells.location,
-      cells.availability,
-      cells.travelPreference,
-      cells.unionMembership,
-      cells.ministry,
-    ],
-  };
-
-  const pendingCells = {
-    [Program.EMCR]: [
-      cells.name,
-      cells.dateApplied,
-      cells.region,
-      cells.location,
-      cells.ics,
-      cells.supervisorApproval,
-      cells.unionMembership,
-      cells.ministry,
-    ],
-    [Program.BCWS]: [
-      cells.name,
-      cells.dateApplied,
-      cells.fireCentre,
-      cells.location,
-      cells.parQ,
-      cells.orientation,
-      cells.ministry,
-    ],
-  };
-  if (isPending) {
-    return pendingCells[program as keyof typeof pendingCells];
-  } else {
-    return activeCells[program as keyof typeof activeCells];
-  }
+    const pendingCells = {
+        [Program.EMCR]: [
+            cells.name,
+            cells.dateApplied,
+            cells.region,
+            cells.location,
+            cells.ics,
+            cells.supervisorApproval,
+            cells.unionMembership,
+            cells.ministry,
+        ],
+        [Program.BCWS]: [
+            cells.name,
+            cells.dateApplied,
+            cells.fireCentre,
+            cells.location,
+            cells.parQ,
+            cells.orientation,
+            cells.ministry,
+        ],
+    };
+    if (isPending) {
+        return pendingCells[program as keyof typeof pendingCells];
+    } else {
+        return activeCells[program as keyof typeof activeCells];
+    }
 };
